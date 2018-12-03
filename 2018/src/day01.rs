e64fed615bd6517acade1efa457d2a341ed49e9e
Janus <janus@insignificancegalore.net>
2018-12-03 11:28:21 +0100

    Solved day01

--- 2018/src/day01.rs
+++ 2018/src/day01.rs
@@ -21,26 +21,19 @@ pub fn part1_01(d: &str) -> i64 {
 }
 
 pub fn part2_01(d: &str) -> i64 {
-//    let shifts = d
-//        .lines()
-//        .map(|a| a.parse::<i64>().unwrap())
-
     let mut fs = HashSet::new();
     let mut s: i64 = 0;
-
-    let xx: i64 = d
+    d
         .lines()
         .map(|a| a.parse::<i64>().unwrap())
         .cycle()
         .map(|a| {s=s+a; s})
-        .take_while(|s| fs.insert(*s))
-        .last().unwrap();
-//        .last()
-//        .unwrap()
-    0
+        .skip_while(|s| fs.insert(*s))
+        .next().unwrap()
 }
 
 
 pub fn run(data: &str) {
     println!("{}", part1_01(&data));
+    println!("{}", part2_01(&data));
 }
