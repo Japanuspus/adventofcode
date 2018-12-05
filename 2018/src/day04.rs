5d70575a976de9aa25599176d4e6120f0d9f7d7a
Janus <janus@insignificancegalore.net>
2018-12-05 08:50:09 +0100

    temp

--- 2018/src/day04.rs
+++ 2018/src/day04.rs
@@ -11,10 +11,6 @@ mod tests {
     }
 }
 
-pub fn part1_01(_d: &str) -> i64{
-    0
-}
-
 pub fn part2_01(_d: &str) -> i64 {
     0
 }
@@ -23,7 +19,7 @@ pub fn part2_01(_d: &str) -> i64 {
 pub enum LogTypes {
   Sleep,
   Wake, 
-  ID(u32),
+  Guard(u32),
 }
 
 #[derive(Debug,PartialEq)]
@@ -32,25 +28,85 @@ pub struct LogLine {
   pub ltype: LogTypes,
 }
 
-named!(parse_datetime<u8>,
-    do_parse!(
-        take!(14) >>
-        mm: be_u8 >>
-        mm
-    )
-);
-
-
-named!(get_greeting<&str,&str>,
-    alt!( tag!("hi") | tag!("bye"))
-);
-
-    // "[1518-11-09 23:58] Guard #853 begins shift";
-
+// "1518-11-09 23:58" -> 58
+fn parse_mm(input: &str) -> Result<u8, std::num::ParseIntError> {
+  u8::from_str_radix(&input[14..], 10)
+}
+fn is_digit(c: char) -> bool {
+  c.is_digit(10)
+}
+fn parse_dec(s: &str) -> Result<u32, std::num::ParseIntError> {
+  u32::from_str_radix(&s, 10)
+}
 
 #[test]
-fn test_get_greeting() {
-    assert_eq!(get_greeting("hi"), Ok(("","hi")));
+fn test_parse_mm() {
+    assert_eq!(parse_mm("1518-11-09 23:58"), Ok(58));
+}
+
+named!(parse_datetime<&str, u8>,
+  map_res!(take!(16), parse_mm)
+);
+#[test]
+fn test_parse_datetime() {
+    assert_eq!(parse_datetime("1518-11-09 23:58"), Ok(("",58)));
+}
+
+named!(parse_msg<&str, LogTypes>,
+  alt!(
+    value!(LogTypes::Sleep, tag!("falls asleep")) |
+    value!(LogTypes::Wake, tag!("wakes up")) |
+    do_parse!(
+      tag!("Guard #") >> 
+      gid: map_res!(take_while!(is_digit), parse_dec) >> 
+      tag!(" begins shift") >> 
+      (LogTypes::Guard(gid))
+    )
+  )
+);
+#[test]
+fn test_parse_msg() {
+    assert_eq!(parse_msg("falls asleep"), Ok(("", LogTypes::Sleep)));
+    assert_eq!(parse_msg("Guard #2887 begins shift"), Ok(("", LogTypes::Guard(2887))));
+}
+
+named!(parse_logline<&str, LogLine>,
+  do_parse!(
+    ltime: delimited!(char!('['), parse_datetime, char!(']')) >>
+    tag!(" ") >>
+    ltype: parse_msg >>
+
+    (LogLine{ltime, ltype})
+  )
+);
+#[test]
+fn test_parse_logline() {
+    assert_eq!(
+      parse_logline("[1518-11-09 23:58] Guard #853 begins shift"),
+      Ok(("",LogLine{ltime: 58, ltype: LogTypes::Guard(853)}))
+    );
+}
+
+#[derive(Debug,PartialEq)]
+pub struct Period {
+  pub guard: u32,
+  pub period: (u8, u8),
+}
+
+pub fn part1_01(d: &str) -> i64{
+    let mut lsorted: Vec<_> = d.lines().collect();
+    lsorted.sort();
+
+    let mut guard_periods: HashMap<u32, Period> = HashMap::new();
+    let mut gid: u32 = 0;
+    let mut t0: u8 =0;
+
+
+    for l in lsorted {
+      println!("> {}", l);
+    }
+
+    0
 }
 
 
