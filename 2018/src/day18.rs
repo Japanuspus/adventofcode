a768dd390b9229a2803ebabd6e25846d9757714c
Janus <janus@insignificancegalore.net>
2019-01-01 17:13:51 +0100

    Rest of inputs

--- 2018/src/day18.rs
+++ 2018/src/day18.rs
@@ -8,6 +8,32 @@ mod tests {
     }
 }
 
+
+fn charkey(c: &u8) -> u8 {
+    match b[1] {
+        b'.' => 0,
+        b'|' => 1,
+        b'#' => 2,
+        _ => panic!
+    }
+}
+
+fn prop(a: & [u8], b: & [u8], c: & [u8]) -> u8 {
+    // Not very efficient, but just fire the big gun
+    let mut counts: [u8;3] = [0,0,0];
+    for v in [a, b, c].iter().flatten() {
+        counts[charkey(v)]+=1;
+    }
+    
+    match b[1] {
+        b'.' => if counts[1]>=3 {b'|'} else {b'.'},
+        b'|' => if counts[2]>=3 {b'#'} else {b'|'},
+        b'#' => if (counts[1]>0 && counts[2]>0) {b'#'} else {b'.'}
+        _ => panic!
+    }
+
+}
+
 pub fn part1_01(_d: &str) -> i64{
     0
 }
