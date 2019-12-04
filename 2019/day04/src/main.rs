1fd90008251f8889fb564b43b88bb302ae530216
Janus <janus@insignificancegalore.net>
2019-12-04 22:31:46 +0100

    Solve day 4 in functional style

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,49 +1,34 @@
 #![allow(unused)]
+use std::iter;
 
-fn is_pin(x: isize) -> bool {
-    let mut cl: Option<u8> = None;
-    let mut double: bool = false;
-    for c in x.to_string().as_bytes() {
-        if let Some(last) = cl {
-            if last == *c {double = true};
-            if *c<last {return false};
-        }
-        cl = Some(*c);
-    }
-    return double
-}
-
-fn is_pin2(x: isize) -> bool {
-    let mut cl: Option<u8> = None;
-    let mut double: bool = false;
-    let mut lastcount: isize = 0;
-
-    for c in x.to_string().as_bytes() {
-        if let Some(last) = cl {
-            if last == *c {
-                lastcount += 1;
-            } else {
-                double = double || lastcount == 1;
-                lastcount = 0;
-            };
-            if *c<last {return false};
-        }
-        cl = Some(*c);
-    }
-    return double || lastcount == 1
+fn run_lengths<'a, T>(a: &'a[T]) -> impl Iterator<Item=usize> + 'a
+where
+    T: std::cmp::PartialEq 
+{
+    a
+    .windows(2)
+    .enumerate()
+    .filter_map(|(i, ab)| if ab[0]==ab[1] {None} else {Some(i as isize)})
+    .chain(iter::once(a.len() as isize - 1)) //append edge at end of slice
+    .scan(-1, |last_pos, pos| { //first edge is before slice
+        let res = pos - *last_pos;
+        *last_pos=pos; 
+        Some(res as usize)
+    })
 }
 
 fn main() {
     let mut input = std::fs::read_to_string("input.txt").expect("Error reading input.txt");
-    let mut lines = input.lines();
-    let mut bounds = lines.next().unwrap().split('-');
+    let mut bounds = input.lines().next().unwrap().split('-');
     let r0: isize=bounds.next().unwrap().parse().unwrap();
     let r1: isize=bounds.next().unwrap().parse().unwrap();
 
-    let n1 = (r0..r1).filter(|x| is_pin(*x)).count();
-    println!("Part 1: {}", n1);
+    let increasing = (r0..r1).map(|x| x.to_string().into_bytes())
+        .filter(|v| v.windows(2).all(|ab| ab[0] <= ab[1]));
 
-    let n2 = (r0..r1).filter(|x| is_pin2(*x)).count();
-    println!("Part 2: {}", n2);
+    println!("Part 1: {}", increasing.clone()
+        .filter(|v| run_lengths(v).any(|k| k>=2)).count());
 
+    println!("Part 2: {}", increasing
+        .filter(|v| run_lengths(v).any(|k| k==2)).count());
 }
