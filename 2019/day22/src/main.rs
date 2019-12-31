7dd336f4686f8b29ff71b9eee421b5d26b21541f
Janus <janus@insignificancegalore.net>
2019-12-31 01:06:40 +0100

    Clean up before realizing I misread part 2

--- 2019/day22/src/main.rs
+++ 2019/day22/src/main.rs
@@ -1,13 +1,7 @@
-#![allow(unused)]
-
-use std::collections::BTreeSet;
-// use std::collections::HashMap;
-// use std::iter;
-// use day11::State; // dep: day11={path="../day11"}
-use modinverse;
-use num_bigint::{BigInt, ToBigInt};
+use num_bigint::ToBigInt;
+use num_bigint::{BigInt};
 use num_traits::cast::{ToPrimitive};
-use num_integer::{Integer};
+// use num_integer::{Integer};
 
 #[derive(Debug)]
 enum Shuffle {
@@ -71,6 +65,67 @@ fn coefficients(cmd: &Shuffle) -> (isize, isize) {
 /// (a1*a0 a1*b0+b1)
 /// (    0        1)
 
+
+fn solve_naive(cmds: &Vec<Shuffle>, n_deck: usize, index_initial: usize) -> usize {
+    let deck0: Vec<u16> = (0..n_deck as u16).collect();
+    let deck = cmds.iter().fold(deck0, |d, c| do_shuffle(&d, c));
+    let (idx,_) = deck.iter().enumerate().filter(|(_, v)| **v == index_initial as u16).next().unwrap();
+    idx
+}
+
+type Map = (BigInt, BigInt);
+
+fn combined_map(cmds: &Vec<Shuffle>) -> Map {
+    cmds.iter()
+    .map(coefficients)
+    .fold(
+        (1.to_bigint().unwrap(), 0.to_bigint().unwrap()),
+        // a1, b1 is applied last
+        |(a0, b0), (a1,b1)| (a0*a1, a1*b0+b1))
+}
+
+fn apply_map_repeated(m: &Map, n_deck: usize,  index_initial: usize, n_repeat: usize) -> usize {
+    let (a_comb, b_comb) = m;
+    let mut a=a_comb % n_deck;
+    let mut b=b_comb % n_deck;
+    let mut idx=index_initial.to_bigint().unwrap();
+    let mut bits=n_repeat;
+    loop {
+        if bits & 1 != 0 {
+            idx = (idx*&a+&b)%n_deck;
+        }
+        bits>>=1;
+        if bits==0 {break}
+        let b2 = b.clone()*&a+&b;
+        let a2 = a.clone()*&a;
+        a=a2 % n_deck;
+        b=b2 % n_deck;
+    }
+    ((idx+n_deck)%n_deck).to_usize().unwrap()
+}
+
+fn apply_map(m: &Map, n_deck: usize, index_initial: usize) -> usize {
+    let (a_comb, b_comb) = m;
+    let index = 
+        (index_initial.to_bigint().unwrap() * a_comb + b_comb) % n_deck;
+    ((index+n_deck)%n_deck).to_usize().unwrap()
+}
+
+fn solve_fast(cmds: &Vec<Shuffle>, n_deck: usize, index_initial: usize) -> usize {
+    apply_map(&combined_map(cmds), n_deck, index_initial)
+}
+
+fn solve_fast_repeated(cmds: &Vec<Shuffle>, n_deck: usize, index_initial: usize, n_repeat: usize) -> usize {
+    let m = combined_map(cmds);
+    apply_map_repeated(&m, n_deck, index_initial, n_repeat)
+}
+
+fn solve_repeated(cmds: &Vec<Shuffle>, n_deck: usize, index_initial: usize, n_repeat: usize) -> usize {
+    let m = combined_map(cmds);
+    (0..n_repeat).fold(index_initial, |idx, _| apply_map(&m, n_deck, idx))
+}
+
+
 fn main() {
     let input = std::fs::read_to_string("input.txt")
         .expect("Error reading input file");
@@ -79,54 +134,21 @@ fn main() {
     .lines()
     .map(|ln| parse_shuffle(ln))
     .collect();
-        
-    let deck0: Vec<u16> = (0..10007).collect();
-    let deck = cmds.iter().fold(deck0, |d, c| do_shuffle(&d, c));
-    let (idx,_) = deck.iter().enumerate().filter(|(i, v)| **v == 2019).next().unwrap();
-    println!("Part 1: {}", idx);
+
+    
+    // Part 1
+    println!("Part 1: {}", solve_naive(&cmds, 10007, 2019));
+    println!("Part 1: {} - fast", solve_fast(&cmds, 10007, 2019));
+
+    println!("Part 1 repeated 7 times: {}", solve_repeated(&cmds, 10007, 2019, 7));
+    println!("Part 1 repeated 7 times: {} - fast", solve_fast_repeated(&cmds, 10007, 2019, 7));
 
     // Part 2
-    // combined map
-    let (a_comb, b_comb) = cmds.iter()
-    .map(coefficients)
-    .fold(
-        (1.to_bigint().unwrap(), 0.to_bigint().unwrap()),
-        // a1, b1 is applied last
-        |(a0, b0), (a1,b1)| (a0*a1, a1*b0+b1));
-    println!("Combined map: {}*x+{}", a_comb, b_comb);
-
     let n_deck:usize = 119315717514047; 
     let index_initial = 2020;
-    let n_repeat:usize = 101_741_582_076_661;
-    //let n_deck:usize = 10007;
-    //let index_initial = 2019;
-    //let n_repeat:usize = 5;
+    let n_repeat:usize = 101741582076661;
 
-    let index = 
-        (index_initial.to_bigint().unwrap() * &a_comb + &b_comb) % n_deck;
-    println!("Part 2, single shuffle round: {}", (index+n_deck)%n_deck);
-
-    //let mut idx=index_initial.to_bigint().unwrap();
-    //for i in 0..n_repeat { idx = (idx*&a+&b)%n_deck; }
-    //println!("part 2 naive: {}", (idx+n_deck)%n_deck);
-
-    let mut a=a_comb % n_deck;
-    let mut b=b_comb % n_deck;
-    let mut idx=index_initial.to_bigint().unwrap();
-    let mut bits=n_repeat;
-    loop {
-        if bits & 1 != 0 {
-            // println!("applying");
-            idx = (idx*&a+&b)%n_deck;
-        }
-        // println!("Bits: {}", &bits);
-        bits>>=1;
-        if bits==0 {break}
-        let b2 = b.clone()*&a+&b;
-        let a2 = a.clone()*&a;
-        a=a2 % n_deck;
-        b=b2 % n_deck;
-    }
-    println!("part 2: {}", (idx+n_deck)%n_deck);
-
+    println!("Part 2: {}", solve_fast_repeated(&cmds, n_deck, index_initial, n_repeat));
 }
+// Attempts 
+// 34228048460648 - too low
