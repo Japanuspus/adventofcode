15b599084fa727346246a50d78512ad0f975e6b2
janus <janus@insignificancegalore.net>
2020-12-03 07:36:41 +0100

    Day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -11,5 +11,13 @@ fn main() -> aoclib::Result<()> {
     println!("part 1: {}", 
         rows.iter().enumerate().filter(|(i, r)| r[(i*3) % r.len()]).count()
     );  
+
+    let dirs: Vec<(usize, usize)> = vec![(1, 1),(3, 1),(5, 1),(7, 1),(1, 2), ];
+    println!("part 2: {}", 
+        dirs.iter().map(|(right, down)| 
+            rows.iter().step_by(*down).enumerate().filter(|(i, r)| r[(i*right) % r.len()]).count()
+        ).product::<usize>()
+    );
+
     Ok(())
 }
