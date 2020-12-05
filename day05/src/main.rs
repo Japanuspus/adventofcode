2e4f3bd80cef20a6292a336c542ed7879e90da1f
Janus <janus@insignificancegalore.net>
2020-12-05 15:50:12 +0100

    Day 5 using sorted vector

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,20 +1,22 @@
-use std::collections::BTreeSet;
 use aoclib;
+use itertools::Itertools;
 
 fn main() -> aoclib::Result<()> {
-    let input = aoclib::get_inputs_pwd()?;
-    let seats: Vec<u32> = input.lines().map(|ln| 
+    let seats: Vec<u32> = aoclib::get_inputs_pwd()?
+    .lines().map(|ln| 
         ln.chars().rev().enumerate().map(|(i, c)| (1<<i)*match c {
             'F'|'L' => 0,
             'B'|'R' => 1,
             _ => panic!(),
-        }).sum::<>()).collect();
+        }).sum()
+    ).sorted().collect();
 
-    println!("Part 1: {}", seats.iter().max().unwrap_or(&0));
+    println!("Part 1: {}", seats.iter().last().unwrap_or(&0));
 
-    let seatset: BTreeSet<_> = seats.iter().collect();
-    println!("Part 2: {}", seats.iter().filter(|&s| 
-            seatset.contains(&(s+2)) && !seatset.contains(&(s+1))).next().unwrap_or(&0));
+    println!("Part 2: {}", 
+        seats.windows(2)
+        .filter_map(|s12| if s12[1]==s12[0]+1 {None} else {Some(s12[0]+1)})
+        .next().unwrap_or(0));
 
     Ok(())
 }
