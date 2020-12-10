6cb53a9e51ef463b42e3aae1dc9fe84ff36a4023
janus <janus@insignificancegalore.net>
2020-12-10 17:00:23 +0100

    Day 10 part 2 in two ways

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use apply::Apply;
 use itertools::Itertools;
-use std::{fs, iter::once};
+use std::{collections::HashMap, fs, iter::once};
 
 fn main() -> Result<()> {
     let input: Vec<_> = fs::read_to_string("input.txt")?
@@ -12,22 +12,44 @@ fn main() -> Result<()> {
 
     println!(
         "Part 1: {}",
-        std::iter::once(input[0])  // diff from input to first adapter
+        std::iter::once(input[0]) // diff from input to first adapter
             .chain(input.windows(2).map(|v| v[1] - v[0]))
             .fold((0, 0), |(a1, a3), d| match d {
                 1 => (a1 + 1, a3),
                 3 => (a1, a3 + 1),
                 _ => (a1, a3),
             })
-            .apply(|(n1, n3)| n1 * (n3 + 1))  // include diff to device
+            .apply(|(n1, n3)| n1 * (n3 + 1)) // include diff to device
     );
 
-    let jolts: Vec<isize> = once(0)
-        .chain(input.iter().cloned())
-        .chain(input.last().map(|v| v+3))
-        .collect();
-
     // fill possibility count starting from rear=1
+    let mut n_map: HashMap<isize, usize> = input.last().iter().map(|&v| (v + 3, 1)).collect();
+    for v in input.iter().rev().cloned().chain(once(0)) {
+        n_map.insert(v, (1..4).filter_map(|dv| n_map.get(&(v + dv))).sum());
+    }
+    println!("Part 2: {}", n_map[&0]);
 
+    // Sometimes oldskool mutable code is just easier to read :)
+    println!(
+        "Part 2: {} as expression",
+        input
+            .iter()
+            .rev()
+            .cloned()
+            .chain(once(0))
+            .scan(
+                input
+                    .last()
+                    .iter()
+                    .map(|&v| (v + 3, 1))
+                    .collect::<HashMap<isize, usize>>(),
+                |m, v| {
+                    ((1..4).filter_map(|dv| m.get(&(v + dv))).sum::<usize>())
+                        .apply(|count| m.entry(v).or_insert(count).apply(|v| Some(*v)))
+                }
+            )
+            .last()
+            .unwrap_or(0)
+    );
     Ok(())
 }
