93fac1a6fcf4aa8726d27fefce168fa46f03251c
Janus <janus@insignificancegalore.net>
2020-12-12 07:00:09 +0100

    Day 12 part 1

--- 2020/day12/src/main.rs
+++ 2020/day12/src/main.rs
@@ -5,17 +5,22 @@ use parse_display::{FromStr};
 // use regex::Regex;
 // use apply::Also;
 
-#[derive(Debug, FromStr)]
-#[display("{cmd}{arg}")]
-struct InputItem {cmd: char, arg: isize}
+const PIOVER180:f32 = 0.01745329251;
 
+#[derive(Debug)] //, FromStr)]
+//#[display("{cmd}{arg}")]
+struct InputItem {cmd: char, arg: isize}
+fn parse_input_item(s: &str) -> Result<InputItem> {
+    let mut ch = s.chars();
+    let cmd = ch.next().ok_or(Error::msg("No cmd"))?;
+    let arg = ch.collect::<String>().parse::<isize>()?;
+    Ok(InputItem{cmd, arg})
+}
 
 fn main() -> Result<()> {
-    println!("{}", "5".parse::<isize>()?);
-
     let input: Vec<InputItem> = fs::read_to_string("input.txt")?
     .lines()
-    .map(|ln| ln.parse().context(format!("While parsing line '{}'", ln)))
+    .map(|ln| parse_input_item(ln).context(format!("While parsing line '{}'", ln)))
     .collect::<Result<_,_>>()?;
 
     #[derive(Debug)]
@@ -29,16 +34,15 @@ fn main() -> Result<()> {
         |mut s, c| {
             match c.cmd {
                 'N' => s.y+=c.arg, // means to move north by the given value.
-                // 'S' => State{x: s.x, y: y-c.arg, heading: heading}, // means to move south by the given value.
-                // 'E' => State{x: s.x+c.arg, y: y, heading: heading}, // means to move east by the given value.
-                // 'W' => State{x: s.x-c.arg, y: y, heading: heading}, // means to move west by the given value.
-                // 'L' => State{x: s.x, y: y, heading: heading+c.arg}, // means to turn left the given number of degrees.
-                // 'R' => State{x: s.x, y: y, heading: heading-c.arg}, // means to turn right the given number of degrees.
-                // 'F' => State{
-                //     x: x+((s.arg as f32)*(s.heading as f32).cos() as isize), 
-                //     y: y+((s.arg as f32)*(s.heading as f32).cos() as isize), 
-                //     heading: heading
-                // }, // means to move forward by the given value in the direction the ship is currently facing.
+                'S' => s.y-=c.arg, // means to move south by the given value.
+                'E' => s.x+=c.arg, // means to move east by the given value.
+                'W' => s.x-=c.arg, // means to move west by the given value.
+                'L' => s.heading+=c.arg, // means to turn left the given number of degrees.
+                'R' => s.heading-=c.arg, // means to turn right the given number of degrees.
+                'F' => {                 // means to move forward by the given value in the direction the ship is currently facing.
+                    s.x+=((c.arg as f32)*((s.heading as f32)*PIOVER180).cos()) as isize;
+                    s.y+=((c.arg as f32)*((s.heading as f32)*PIOVER180).sin()) as isize;
+                }, 
                 _ => panic!("Unexpected cmd")
             };
             s
