46362bf3592db84d65f18bfdf29025ff446d8c83
Janus <janus@insignificancegalore.net>
2020-12-19 21:10:03 +0100

    Parser fix for rule 42

--- 2020/day19/src/main.rs
+++ 2020/day19/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use nom::branch::alt;
 use nom::sequence::delimited;
 use nom::character::complete::{anychar, char};
@@ -8,7 +9,7 @@ use nom::combinator::recognize;
 use nom::combinator::map_res;
 use nom::bytes::complete::tag;
 use nom::sequence::separated_pair;
-use nom::{IResult};
+use nom::{IResult, Finish};
 use std::collections::HashMap;
 use std::fs;
 use anyhow::Result;
@@ -19,43 +20,45 @@ use anyhow::Error;
 // use apply::Also;
 // use num::{BigInt, Integer};
 
-type RuleList = (usize, usize);
 #[derive(Debug)]
 enum Rule {
     single(char),
-    rule_lists(Vec<RuleList>)
+    rule_lists(Vec<Vec<usize>>)
 }
 
+#[derive(Debug)]
 struct Input<'a> {rules: HashMap<usize, Rule>, messages: Vec<&'a str>}
 
 fn parse_number(input : &str) -> IResult<&str, usize> {
     map_res(recognize(digit1), str::parse)(input)
 }
 
-fn parse_rule(s: &str) -> IResult<&str, (usize, Rule)> {
-    //let number = map_res(recognize(digit1), str::parse);
-    let rule_list = separated_pair(parse_number, tag(" "), parse_number);
+fn parse_rule(s: &str) -> Result<(usize, Rule)> {
+    let rule_list = separated_list1(tag(" "), parse_number);
     let rule1 = map(separated_list1(tag(" | "), rule_list), |v| Rule::rule_lists(v));
     let rule2 = map(delimited(char('"'), anychar, char('"')), |c| Rule::single(c)); 
     let rule = alt((rule1, rule2));
     let mut line = separated_pair(parse_number, tag(": "), rule);
-    line(s)
+    let res = line(s)
+    .finish()
+    .map(|(_, r)| r)
+    // https://github.com/Geal/nom/blob/master/doc/nom_recipes.md#implementing-fromstr
+    .map_err(|nom::error::Error { input, code }| nom::error::Error{input: input.to_string(), code});
+    Ok(res?)
 }
 
-
-// fn parse(s: &str) -> Result<Input> {
-//     let mut parts = s.split("\n\n");
-//     let rules: HashMap<_,_> = parts.next().ok_or(Error::msg("no rules"))?.lines().map(|ln| parse_rule(ln)).collect::<Result<_,_>>()?;
-//     let messages = parts.next().ok_or(Error::msg("no rules"))?.lines().collect();
-//     Ok(Input{rules, messages})
-// }
+fn parse(s: &str) -> Result<Input> {
+    let mut parts = s.split("\n\n");
+    let rules: HashMap<_,_> = parts.next().ok_or(Error::msg("no rules"))?.lines().enumerate()
+    .map(|(i, ln)| parse_rule(ln).with_context(|| format!("While parsing line {}: {}", i, ln))).collect::<Result<_,_>>()?;
+    let messages = parts.next().ok_or(Error::msg("no rules"))?.lines().collect();
+    Ok(Input{rules, messages})
+}
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("input.txt")?;
+    let inputs = fs::read_to_string("input.txt")?;
+    let input = parse(&inputs);
  
-    println!("Part 1: {}", input.len());
-    println!("{:?}", parse_rule("2: \"a\""));
-    println!("{:?}", parse_rule("4: 12 3"));
-    println!("{:?}", parse_rule("6: 12 3 | 4 8"));
+    println!("Part 1: {:?}", &input);
     Ok(())
 }
