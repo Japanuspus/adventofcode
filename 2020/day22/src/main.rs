89df833ece61581673297c1a48e87c55f5e3fda7
janus <janus@insignificancegalore.net>
2020-12-22 08:47:57 +0100

    Day 22 part 2

--- 2020/day22/src/main.rs
+++ 2020/day22/src/main.rs
@@ -1,18 +1,8 @@
 use std::{collections::{HashSet, VecDeque}, fs};
 use anyhow::Result;
-// use itertools::Itertools;
-// use anyhow::Context;
-// use itertools::Itertools;
-// use parse_display::{FromStr};
-// use regex::Regex;
-// use apply::Also;
-// use num::{BigInt, Integer};
-
-//#[derive(Debug, FromStr)]
-//#[display("{key}:{value}")]
-//struct InputItem {key: String, value: String}
 
 type Deck = VecDeque<u8>;
+
 fn play_normal(mut p1: Deck, mut p2: Deck) -> (bool, Deck, Deck) {
     while (p1.len()>0) & (p2.len()>0) {
         let c1 = p1.pop_front().unwrap();
@@ -26,13 +16,13 @@ fn play_normal(mut p1: Deck, mut p2: Deck) -> (bool, Deck, Deck) {
     (p1.len()>0, p1, p2)
 }
 
-fn play_recursive(mut p1: Deck, mut p2: Deck, old_games: &mut HashSet<[Deck;2]>) -> (bool, Deck, Deck) {
-    if !old_games.insert([p1.clone(), p2.clone()]) {
-        println!("Repeat game: {:?}   <-->   {:?}", p1, p2);
-        return (true, p1, p2)
-    }
-    println!(">>   {:?}   <-->   {:?}", p1, p2);
+fn play_recursive(mut p1: Deck, mut p2: Deck) -> (bool, Deck, Deck) {
+    let mut old_games: HashSet<[Deck;2]> = HashSet::new();
     while (p1.len()>0) & (p2.len()>0) {
+        if !old_games.insert([p1.clone(), p2.clone()]) {        
+            return (true, p1, p2)
+        }
+    
         let c1 = p1.pop_front().unwrap();
         let c2 = p2.pop_front().unwrap();
         let iwin: bool = if ((c1 as usize)<=p1.len()) & ((c2 as usize)<=p2.len()) {
@@ -40,7 +30,6 @@ fn play_recursive(mut p1: Deck, mut p2: Deck, old_games: &mut HashSet<[Deck;2]>)
             play_recursive(
                 p1.iter().take(c1 as usize).cloned().collect(), 
                 p2.iter().take(c2 as usize).cloned().collect(), 
-                old_games
             ).0
         } else {
             c1>c2
@@ -49,18 +38,15 @@ fn play_recursive(mut p1: Deck, mut p2: Deck, old_games: &mut HashSet<[Deck;2]>)
             p1.push_back(c1); p1.push_back(c2);
         } else {
             p2.push_back(c2); p2.push_back(c1);
-        }
-        println!("      {:?}   <-->   {:?}", &p1, &p2);
+        }    
     };
     let iwin = p1.len()>0;
-    println!("<< {} {:?}   <-->   {:?}", &iwin, &p1, &p2);
     (iwin, p1, p2)
 }
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("input_part1.txt")?;
+    let input = fs::read_to_string("input.txt")?;
     let decks: Vec<VecDeque<u8>> = input.split("\n\n").map(|pt| {
-        println!("{:?}", pt);
         pt.lines().skip(1).filter_map(|ln| ln.parse::<u8>().ok()).collect()
     }).collect();
 
@@ -68,8 +54,8 @@ fn main() -> Result<()> {
     let w = if p1.len()==0 {&p2} else {&p1};
     println!("Part 1: {}", w.iter().rev().enumerate().map(|(i, c)| (i+1)*(*c as usize)).sum::<usize>());
 
-    let (_, p1, p2) = play_recursive(decks[0].clone(), decks[1].clone(), &mut HashSet::new());
-    let w = if p1.len()==0 {&p2} else {&p1};
+    let (iwin, p1, p2) = play_recursive(decks[0].clone(), decks[1].clone());
+    let w = if iwin {&p1} else {&p2};
     println!("Part 2: {}", w.iter().rev().enumerate().map(|(i, c)| (i+1)*(*c as usize)).sum::<usize>());
 
     Ok(())
