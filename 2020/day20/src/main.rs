ba0650e6b2d3075ed6c17500c0140a86781c2f0d
janus <janus@insignificancegalore.net>
2020-12-23 14:33:18 +0100

    Day 20 part 2

--- 2020/day20/src/main.rs
+++ 2020/day20/src/main.rs
@@ -2,7 +2,7 @@
 
 use anyhow::Result;
 // use apply::Apply;
-use itertools::{Itertools, iterate};
+use itertools::{iterate, Itertools};
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, one_of};
 use nom::combinator::{map, map_res, recognize};
@@ -31,9 +31,9 @@ impl RawTile {
         self.pixels.reverse();
     }
 
-    fn rotate(&mut self) { 
+    fn rotate(&mut self) {
         self.pixels = transpose_vectors(self.pixels.clone());
-        self.flip();  
+        self.flip();
     }
 }
 
@@ -71,7 +71,7 @@ fn parse_raw_tiles(s: &str) -> Result<Vec<RawTile>> {
 struct Tile {
     id: usize,
     pixels: Vec<Vec<bool>>, // includes edges
-    sides: [Vec<bool>; 4], //clockwise from top
+    sides: [Vec<bool>; 4],  //clockwise from top
 }
 
 impl Tile {
@@ -106,10 +106,10 @@ fn all_symmetric(r: &RawTile) -> Vec<Tile> {
     for _ in 0..2 {
         for _ in 0..4 {
             v.push(Tile::new(buf.clone()));
-            buf.rotate(); 
+            buf.rotate();
         }
         buf.flip();
-    };
+    }
     v
 }
 
@@ -182,6 +182,70 @@ fn check_game(game: &Game) {
     assert!(game.tiles[0].matches(&game.tiles[4], 0));
 }
 
+fn find_monsters(solution: &Vec<Vec<bool>>, monster: &Vec<Vec<bool>>) -> Option<usize> {
+    let sm = solution.len();
+    let sn = solution[0].len();
+    let mm = monster.len();
+    let mn = monster[0].len();
+
+    let mut monster_count = 0;
+    for i in 0..(sm - mm + 1) {
+        for j in 0..(sn - mn + 1) {
+            // check mask at i,j
+            if solution[i..(i + mm)]
+                .iter()
+                .zip(monster)
+                .flat_map(|(row, mrow)| row[j..(j + mn)].iter().zip(mrow))
+                .all(|(pixel_val, monster_val)| !(monster_val & !pixel_val))
+            {
+                println!("Monster at {}, {}", i, j);
+                monster_count += 1;
+            }
+        }
+    }
+
+    fn count_true(v: &Vec<Vec<bool>>) -> usize {
+        v.iter().map(|r| r.iter().filter(|&v| *v).count()).sum()
+    }
+
+    if monster_count > 0 {
+        let n_solution = count_true(solution);
+        let n_monster = count_true(monster);
+        let res = n_solution - monster_count * n_monster;
+        println!(
+            "Roughness: {} - {}*{} = {}",
+            n_solution, monster_count, n_monster, res
+        );
+        Some(res)
+    } else {
+        None
+    }
+}
+
+fn solution_bitmap(game: &Game, s: Vec<&Tile>) -> Vec<Vec<bool>> {
+    s.chunks_exact(game.n)
+        .flat_map(|row| {
+            (1..(game.ns - 1)).map(move |ri| {
+                row.iter()
+                    .flat_map(|t| t.pixels[ri][1..(game.ns - 1)].iter())
+                    .cloned()
+                    .collect()
+            })
+        })
+        .collect()
+}
+
+fn print_bitmap(b: &Vec<Vec<bool>>) {
+    for row in b.iter() {
+        println!(
+            "{}",
+            row.iter()
+                .map(|c| if *c { '#' } else { '.' })
+                .collect::<String>()
+        );
+    }
+}
+
 fn main() -> Result<()> {
     let input = fs::read_to_string("input.txt")?;
     let raw_tiles: Vec<RawTile> = parse_raw_tiles(&input[..])?;
@@ -205,5 +269,32 @@ fn main() -> Result<()> {
             .product::<usize>()
     );
 
+    print_bitmap(&solution_bitmap(&game, solutions[0].clone()));
+
+    let monster_s: &str = "..................#.
+     #....##....##....###
+     .#..#..#..#..#..#...";
+    let monster: Vec<Vec<bool>> = monster_s
+        .lines()
+        .map(|ln| {
+            ln.chars()
+                .filter_map(|c| {
+                    if c == '#' {
+                        Some(true)
+                    } else {
+                        if c == '.' {
+                            Some(false)
+                        } else {
+                            None
+                        }
+                    }
+                })
+                .collect()
+        })
+        .collect();
+
+    for s in solutions {
+        find_monsters(&solution_bitmap(&game, s), &monster);
+    }
     Ok(())
 }
