59562eaff74426ca808547cdaf63c9b6a9d99311
Janus <janus@insignificancegalore.net>
2021-12-06 06:26:59 +0100

    Day 6 clean up and use rotate

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -2,6 +2,16 @@ use anyhow::{Result, Context};
 use std::fs;
 
 
+fn part1(input: &[usize], steps: usize) -> usize {
+    let mut counts: [usize;9] = [0; 9];
+    for f in input.iter() {counts[*f]+=1;}
+    for _ in 0..steps {
+        counts.rotate_left(1);
+        counts[6]+=counts[8];
+    }
+    counts.iter().sum::<usize>()
+}
+
 fn main() -> Result<()> {
     let input_s = fs::read_to_string("input.txt")?;
     let input: Vec<usize> = input_s
@@ -10,34 +20,7 @@ fn main() -> Result<()> {
         .map(|s| s.parse().with_context(|| format!("Parsing {}", s)))
         .collect::<Result<_,_>>()?;
 
-
-    let mut counts: [usize;9] = [0; 9];
-    for f in input.iter() {counts[*f]+=1;}
-    for _ in 0..80 {
-        let mut new_counts: [usize;9] = [0; 9];
-        new_counts[0] = counts[1];
-        for v in 0usize..8usize {
-            new_counts[v] = counts[v+1];
-        }
-        new_counts[6]+=counts[0];
-        new_counts[8]+=counts[0];
-        counts=new_counts;
-    }
-    println!("Part 1: {}", counts.iter().sum::<usize>());
-
-
-    let mut counts: [usize;9] = [0; 9];
-    for f in input.iter() {counts[*f]+=1;}
-    for _ in 0..256 {
-        let mut new_counts: [usize;9] = [0; 9];
-        new_counts[0] = counts[1];
-        for v in 0usize..8usize {
-            new_counts[v] = counts[v+1];
-        }
-        new_counts[6]+=counts[0];
-        new_counts[8]+=counts[0];
-        counts=new_counts;
-    }
-    println!("Part 2: {}", counts.iter().sum::<usize>());
+    println!("Part 1: {}", part1(&input, 80));
+    println!("Part 2: {}", part1(&input, 256));
     Ok(())
 }    
