4a9388b5a4269331e07b3a76d6bc291e02913b1d
Janus <janus@insignificancegalore.net>
2021-12-12 08:51:58 +0100

    Day 12 as solved

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -31,6 +31,29 @@ impl <'a> Route <'a> {
     }
 }
 
+#[derive(Debug)]
+struct RouteT <'a> {
+    path: Vec<&'a str>,
+    at: &'a str,
+    visited: HashSet<&'a str>,
+    twice: Option<&'a str>,
+}
+
+impl <'a> RouteT <'a> {
+    fn new(at: &'a str) -> Self {
+        RouteT{at, visited: HashSet::new(), twice: None, path: vec![at]}
+    }
+    fn goto(&self, dst: &'a str) -> Self {
+        let mut path = self.path.clone();
+        path.push(dst);
+        Self{
+            at: dst, visited: self.visited.clone(), twice: self.twice,
+            path
+        }
+    }
+}
+
+
 fn solution(input_s: &str) -> Result<()> {
     let input: Vec<Vec<&str>> = input_s
         .trim()
@@ -42,7 +65,6 @@ fn solution(input_s: &str) -> Result<()> {
         caves.entry(a.to_string()).or_insert_with(|| Cave::from_name(a)).push(b)
     }
     println!("{:?}", caves);
-
 
     let mut p1: usize = 0;
     let mut routes: Vec<Route> = vec![Route::new("start")];
@@ -64,8 +86,34 @@ fn solution(input_s: &str) -> Result<()> {
                 } )
         );
     }
-    println!("Part 1: {}", p1);
-    println!("Part 2: {}", 0);
+    println!("Part 1: {}\n", p1);
+
+    let mut p2: usize = 0;
+    let mut routes: Vec<RouteT> = vec![RouteT::new("start")];
+    while let Some(mut r) = routes.pop() {
+        //println!("R: {:?}\nRoutes: {:?}", &r, &routes);
+        let a = caves.get(r.at).unwrap();
+        if a.is_end {
+            println!("Finished route: {:?}", r);
+            p2+=1;
+            continue;
+        }
+        if a.is_small { 
+            let first_time = r.visited.insert(r.at);
+            if !first_time {
+                r.twice = Some(r.at);
+            }
+        }
+        routes.extend(
+            a.edges.iter().filter_map(|dst| 
+                if (r.visited.contains(&dst[..]) && r.twice.is_some()) || dst == "start" {
+                    None
+                } else {
+                    Some(r.goto(dst))
+                } )
+        );
+    }
+    println!("Part 2: {}", p2);
     Ok(())
 }
 
@@ -73,6 +121,6 @@ fn main() -> Result<()> {
     println!("** TEST **");
     solution(&fs::read_to_string("test00.txt")?)?;
     println!("\n** INPUT **");
-    //solution(&fs::read_to_string("input.txt")?)?;
+    solution(&fs::read_to_string("input.txt")?)?;
     Ok(())
 }
