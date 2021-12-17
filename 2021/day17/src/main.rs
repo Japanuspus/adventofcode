c3acbd8203943506b1d7619f0ea9f98dc05b5341
Janus <janus@insignificancegalore.net>
2021-12-17 12:45:58 +0100

    Day 17 as solved

--- 2021/day17/src/main.rs
+++ 2021/day17/src/main.rs
@@ -18,10 +18,71 @@ struct Target {
     y: Range,
 }
 
-fn ystep(dist: isize) -> f32 {
+
+// y speeds after starting from 0
+//  n = 0   1   2   3
+// vy = 0   1   2   3
+//  y = 0   0   1   3   6  = m*(m+1)/2  m = n-1     (at end of step)
+//  n = sqrt(2y+.25)-.5
+
+fn y_step(dist: isize) -> f32 {
     let y: f32 = dist as f32;
     (2.0*y+0.25).sqrt()-0.5
 }
+
+
+/// Possible step counts after starting with vy
+fn y_range(vy: isize, r: &Range) -> Option<(usize, usize)> {
+    // negative speed, fictive start at y_top
+    // -1: we started here one step ago (no motion first step)
+    // -2: we started one above two steps ago
+    let y_top = vy*(vy+1)/2;
+    let n1f = (vy as f32) + 1.0 + y_step(y_top - r.b);
+    let n2f = (vy as f32) + 1.0 + y_step(y_top - r.a);
+    let n1 = n1f.ceil() as usize;
+    let n2 = n2f.floor() as usize;
+    //dbg!((vy, &r, n1f, n1, n2f, n2));
+    if n2<n1 {
+        None
+    } else {
+        Some((n1, n2))
+    }
+}
+
+//      1    2     3      4    5    6
+//      1    0    -1     -2   -3   -4
+//  0   1    1     0     -2   -5   -9
+#[test]
+fn test_y_range() {
+    assert_eq!(y_range(1, &Range{a:-4, b:-3}), None);
+    assert_eq!(y_range(1, &Range{a:-4, b:-2}), Some((4, 4)));
+    assert_eq!(y_range(1, &Range{a:-5, b:-2}), Some((4, 5)));
+    assert_eq!(y_range(1, &Range{a:-5, b:-3}), Some((5, 5)));
+}
+
+//       1      2      3  
+//      -1    -2     -3
+//  0   -1    -3     -6
+#[test]
+fn test_y_range_negv() {
+    assert_eq!(y_range(-1, &Range{a:-5, b:-4}), None);
+    assert_eq!(y_range(-1, &Range{a:-6, b:-4}), Some((3, 3)));
+    assert_eq!(y_range(-1, &Range{a:-6, b:-3}), Some((2, 3)));
+}
+
+
+//      1    2     3      4   
+//      0    -1     -2   -3   
+//  0   0    -1     -3   -6   
+#[test]
+fn test_y_range_zerov() {
+    assert_eq!(y_range(0, &Range{a:-5, b:-4}), None);
+    assert_eq!(y_range(0, &Range{a:-6, b:-4}), Some((4, 4)));
+    assert_eq!(y_range(0, &Range{a:-5, b:-3}), Some((3, 3)));
+    assert_eq!(y_range(0, &Range{a:-6, b:-3}), Some((3, 4)));
+}
+
+
 
 fn solution(input_s: &str) -> Result<()> {
     let input: Target = input_s
@@ -30,69 +91,47 @@ fn solution(input_s: &str) -> Result<()> {
     let t = input;
     println!("Input: {}", t);
 
-    // y speeds after starting from 0
-    //  n = 0   1   2   3
-    // vy = 0   1   2   3
-    //  y = 0   0   1   3   6  = m*(m+1)/2  m = n-1     (at end of step)
-    //  n = sqrt(2y+.25)-.5
     let mut y_max = 0;
     for vy in 0..1000 {
-        let y_top = vy*(vy+1)/2;
-        let n1f = (vy as f32) + 1.0 + ystep(y_top - t.y.b);
-        let n2f = (vy as f32) + 1.0 + ystep(y_top - t.y.a);
-        let n1 = n1f.ceil();
-        let n2 = n2f.floor();
-        if n2<n1 {
-            continue
+        if let Some(_) = y_range(vy, &t.y) {
+            y_max = vy*(vy+1)/2;
         }
-        y_max = vy*(vy+1)/2;
-        // println!("Vy {} (max: {}): {}..={} ({}--{})", vy, y_max, n1, n2, n1f, n2f);
     }
     println!("Part 1: {}", y_max);
 
 
     // limits on vx
     let vxmax = t.x.b+1; 
-    let vxmin = ystep(t.x.a).floor() as isize - 1;
+    let vxmin = y_step(t.x.a).floor() as isize - 1;
 
     // Include negative initial y...
     let mut p2 = 0;
-    for vy in -100..1000 {
-        // negative speed, fictive start at y_top
-        // -1: we started here one step ago (no motion first step)
-        // -2: we started one above two steps ago
-        let y_top = vy*(vy+1)/2; // valid for both signs
-        let n1f = (vy as f32) + 1.0 + ystep(y_top - t.y.b);
-        let n2f = (vy as f32) + 1.0 + ystep(y_top - t.y.a);
-        let n1 = n1f.ceil() as isize;
-        let n2 = n2f.floor() as isize;
-        if n2<n1 {
-            continue
-        }
-        
-        for vx0 in vxmin..=vxmax {
-            let mut vx = vx0;
-            let mut x = 0;
-            for i in 1..=n2 {
-                if vx>0 {
-                    x+=vx;
-                    vx-=1;
-                };
-                if x>t.x.b {break}
-                if i<n1 {continue}
-                if x>=t.x.a {
-                    // solution
-                    //println!("Vy {}: {}..={} ({}--{})", vy, n1, n2, n1f, n2f);
-                    //println!("Solution: {}, {} at i={}", vx0, vy, i);
-                    //print!("({}, {}), ", vx0, vy);
-                    p2+=1;
-                    break
+    for vy in -300..1000 {
+        if let Some((n1, n2)) = y_range(vy, &t.y) {
+            for vx0 in vxmin..=vxmax {
+                let mut vx = vx0;
+                let mut x = 0;
+                for i in 1..=n2 {
+                    if vx>0 {
+                        x+=vx;
+                        vx-=1;
+                    };
+                    if x>t.x.b {break}
+                    if i<n1 {continue}
+                    if x>=t.x.a {
+                        // solution
+                        //println!("Vy {}: {}..={} ({}--{})", vy, n1, n2, n1f, n2f);
+                        //println!("Solution: {}, {} at i={}", vx0, vy, i);
+                        //print!("({}, {}), ", vx0, vy);
+                        p2+=1;
+                        break
+                    }
+    
                 }
-
+   
             }
-
+    
         }
-
     }
     println!("\nPart 2: {}", p2);
     Ok(())
