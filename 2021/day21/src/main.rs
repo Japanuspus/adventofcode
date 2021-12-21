0dcc6cb89008182798804d7f5da50414d60d0708
Janus <janus@insignificancegalore.net>
2021-12-21 09:23:09 +0100

    Day 21 slight cleanup

--- 2021/day21/src/main.rs
+++ 2021/day21/src/main.rs
@@ -1,8 +1,6 @@
-#![allow(unused_imports, dead_code)]
-
 use anyhow::{Result, Context};
 use itertools::Itertools;
-use std::{fs, iter::Cycle, collections::HashMap};
+use std::{fs, collections::HashMap};
 
 fn parse(input_s: &str) -> Result<Vec<u32>> {
     input_s
@@ -38,6 +36,13 @@ struct GameState {
 }
 
 impl GameState {
+    fn new(input: &Vec<u32>) -> Self {
+        Self {
+            pos_minus_1: [input[0] as u8 -1, input[1] as u8 -1],
+            score: [0;2],
+        }
+    }
+
     fn play(&self, i: usize, roll: u8) -> Self {
         let mut s = self.clone();
         let pm1 = (s.pos_minus_1[i]+roll) % 10;
@@ -56,10 +61,7 @@ fn part2(input: &Vec<u32>) {
     }
 
     let mut states: Counts = HashMap::new();
-    states.insert(GameState{
-        pos_minus_1: [input[0] as u8 -1, input[1] as u8 -1],
-        score: [0;2],
-    }, 1);
+    states.insert(GameState::new(input), 1);
 
     let mut wins = [0usize;2];
     while states.len() > 0 {
@@ -79,20 +81,15 @@ fn part2(input: &Vec<u32>) {
             states = states_next;
         }
     }
-    //dbg!(wins);
     println!("Part 2: {}", wins.iter().max().unwrap());
 }
 
-fn solve(n: &str) -> Result<()> {
-    println!("** FILE: {} **", n);
-    let input = parse(&fs::read_to_string("test00.txt")?)?;
-    part1(&input);
-    part2(&input);
-    Ok(())
-}
-
 fn main() -> Result<()> {
-    solve("test00.txt")?; 
-    solve("input.txt")?; 
+    for n in ["test00.txt", "input.txt"] {
+        println!("** FILE: {} **", n);
+        let input = parse(&fs::read_to_string(n)?)?;
+        part1(&input);
+        part2(&input);   
+    }
     Ok(())
 }
