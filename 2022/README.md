deb6ff9e250c41fe5dda2a43751ac3d1009eb031
Janus <janus@insignificancegalore.net>
2022-11-30 20:22:46 +0100

    Ready for 2022

---
+++ 2022/README.md
@@ -0,0 +1,11 @@
+# Advent of code 2022 in rust
+
+This year I was tempted to have a go at array-programming with APL/[BQN](https://mlochbaum.github.io/BQN/), but I will at least start out with rust. Have not built much in rust over the last year, so this will be a good opportunity to freshen up.
+
+As for the previous years, I will try to timebox my solutions were all done on the days they are released.
+
+## General tools
+
+Will be using `aocprep` from 2020 again.
+
+## Day 1
---
+++ 2022/skeleton/main.rs
@@ -0,0 +1,39 @@
+#![allow(unused_imports, dead_code)]
+
+use anyhow::{Result, Context};
+use std::fs;
+
+// use parse_display::{Display, FromStr};
+
+// #[derive(Display, FromStr, PartialEq, Debug)]
+// enum Direction {
+//     #[display("forward")]
+//     Forward,
+// }
+
+// #[derive(Debug, Display, FromStr)]
+// #[display("{direction} {distance}")]
+// struct Step {
+//     direction: Direction,
+//     distance: i32,
+// }
+
+fn solution(input_s: &str) -> Result<()> {
+    let input: Vec<i32> = input_s
+        .trim()
+        .split("\n")
+        .map(|s| s.parse().with_context(|| format!("Parsing {}", s)))
+        .collect::<Result<_,_>>()?;
+
+    println!("Part 1: {}", input.len());
+    println!("Part 2: {}", 0);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("** TEST **");
+    solution(&fs::read_to_string("test00.txt")?)?;
+    println!("\n** INPUT **");
+    solution(&fs::read_to_string("input.txt")?)?;
+    Ok(())
+}
