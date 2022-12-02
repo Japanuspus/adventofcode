49f3beb9c25ea6d4feb0b58ba4f1b7fe4f3e4097
Janus <janus@insignificancegalore.net>
2022-12-02 06:44:19 +0100

    Day 02 cleaned up

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -3,28 +3,6 @@
 use anyhow::{Result, Context};
 use std::fs;
 
-// use parse_display::{Display, FromStr};
-
-// #[derive(Display, FromStr, PartialEq, Debug)]
-// enum Direction {
-//     #[display("forward")]
-//     Forward,
-// }
-
-// #[derive(Debug, Display, FromStr)]
-// #[display("{direction} {distance}")]
-// struct Step {
-//     direction: Direction,
-//     distance: i32,
-// }
-
-// Rock: 0, Paper: 1, Scissor: 2
-// (b-a+1).rem_euclid(3)-1 : 0 on tie, 1 if b wins, -1 if a wins
-
-fn game(a: i32, b: i32) -> i32 {
-    (b-a+1).rem_euclid(3)-1
-}
-
 fn solution(input_s: &str) -> Result<()> {
     let input: Vec<(i32, i32)> = input_s
         .trim()
@@ -35,8 +13,11 @@ fn solution(input_s: &str) -> Result<()> {
         ))
         .collect();
 
-    let part1:i32 = input.iter().map(|(a, b)| (b+1) + 3*(game(*a, *b)+1)).sum();
-    let part2:i32 = input.iter().map(|(a, x)| 3*x + 1+(a+x-1).rem_euclid(3)).sum();
+    // Rock: 0, Paper: 1, Scissor: 2
+    // (b-a+1).rem_euclid(3)-1 : 0 on tie, 1 if b wins, -1 if a wins
+
+    let part1:i32 = input.iter().map(|(a, b)| (b+1) + 3*(b-a+1).rem_euclid(3)).sum();
+    let part2:i32 = input.iter().map(|(a, x)| ((a+x-1).rem_euclid(3)+1) + 3*x).sum();
     
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
