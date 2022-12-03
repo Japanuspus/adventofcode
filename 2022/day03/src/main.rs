4c837359b3890f7703ae219dc2bfc40c9cf26612
Janus <janus@insignificancegalore.net>
2022-12-03 06:46:43 +0100

    Day 02 as solved

---
+++ 2022/day03/src/main.rs
@@ -0,0 +1,54 @@
+#![allow(unused_imports, dead_code)]
+
+use anyhow::{Result, Context};
+use std::fs;
+use std::collections::HashSet;
+use itertools::Itertools;
+
+fn solution(input_s: &str) -> Result<(String, String)> {
+    let part1: usize = input_s
+        .trim()
+        .split("\n")
+        .map(|s| s.as_bytes())
+        .map(|s| {
+            let n = s.len()/2;
+            let c1 = s[..n].iter().cloned().collect::<HashSet<_>>();
+            let c2 = s[n..].iter().cloned().collect::<HashSet<_>>();
+            let c = c1.intersection(&c2).cloned().next().unwrap();
+            let p = if c>=b'a' {1+c-b'a'} else {27+c-b'A'};
+            //println!("{} -> {}", c as char, p);
+            p
+        } as usize)
+        .sum();
+
+    let part2: usize = input_s
+        .trim()
+        .split("\n")
+        .map(|s| s.as_bytes().iter().cloned().collect::<HashSet<_>>())
+        .tuples::<(_,_,_)>()
+        .map(|(a,b,cs)| {
+            let ab = a.intersection(&b).cloned().collect::<HashSet<_>>();
+            let c = ab.intersection(&cs).cloned().next().unwrap();
+            let p = if c>=b'a' {1+c-b'a'} else {27+c-b'A'};
+            // println!("{} -> {}", c as char, p);
+            p
+        } as usize)
+        .sum();
+
+        Ok((part1.to_string(), part2.to_string()))
+}
+
+#[test]
+fn test_solution() -> Result<()> {
+    let res=solution(&fs::read_to_string("test00.txt")?)?;
+    println!("Part 1: {}\nPart 2: {}", res.0, res.1);
+    assert!(res.0=="157");
+    assert!(res.1=="70");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let res=solution(&fs::read_to_string("input.txt")?)?;
+    println!("Part 1: {}\nPart 2: {}", res.0, res.1);
+    Ok(())
+}
