c0deeae79cc3bedb0e5d0547b15143c8482c3755
Janus <janus@insignificancegalore.net>
2022-12-09 05:59:24 +0100

    Day 8 parallel implementation

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -1,11 +1,13 @@
 #![allow(unused_imports, dead_code)]
 
 use anyhow::{Result, Context};
+use itertools::{InterleaveShortest, iterate};
 use std::collections::{HashMap, HashSet};
 use std::{fs, time::Instant};
 use vecmath::{vec2_add, vec2_cast, vec2_scale};
+use rayon::prelude::*;
 
-fn solution(input_s: &str) -> Result<(String, String)> {
+fn solution_0(input_s: &str) -> Result<(String, String)> {
     let input: Vec<Vec<i8>> = input_s
         .trim()
         .split("\n")
@@ -55,19 +57,68 @@ fn solution(input_s: &str) -> Result<(String, String)> {
     Ok((part1.to_string(), part2.to_string()))
 }
 
+// Handle each point independently to allow simple parallelization
+// This has worse complexity than origininal solution, but might be faster.
+fn solution_1(input_s: &str) -> Result<(String, String)> {
+    let input: HashMap<[i32;2],i8> = input_s.trim().split("\n").enumerate()
+        .flat_map(|(i, s)| 
+            s.as_bytes().iter().enumerate()
+            .map(move |(j, b)| ([i as i32, j as i32], (b-b'0') as i8))
+        ).collect();
+    let dirs = [[0,1], [1,0], [0,-1], [-1,0]];
+
+    let part1 = input.par_iter().filter(|(&p0, &v)| { 
+        dirs.iter().any(|d|
+            // return true if visible in direction d 
+            iterate(p0, |&p| vec2_add(p, *d))
+            .skip(1)
+            .map_while(|p| input.get(&p))
+            .all(|&o| o<v)
+        )
+    }).count();    
+
+    let part2: usize = input.par_iter().map(|(&p0, &v)| { 
+        dirs.iter().map(|d|
+            iterate(p0, |&p| vec2_add(p, *d))
+            .skip(1).enumerate()
+            .find_map(|(i, p)|{
+                if let Some(o) = input.get(&p) {
+                    if o<&v {None} else {Some(i+1)}
+                } else {
+                    Some(i)
+                }
+            })
+            .unwrap()
+        ).product()
+    }).max().unwrap();    
+
+    Ok((part1.to_string(), part2.to_string()))
+}
+
+type Solution = dyn Fn(& str) -> Result<(String, String)>;
+const SOLUTIONS: [(&str, &Solution);2] = [
+    ("Original", &solution_0), 
+    ("Parallel", &solution_1),
+];
+
 #[test]
 fn test_solution() -> Result<()> {
-    let res=solution(&fs::read_to_string("test00.txt")?)?;
-    println!("Part 1: {}\nPart 2: {}", res.0, res.1);
-    assert!(res.0=="21");
-    assert!(res.1=="8");
+    for (_, solution) in SOLUTIONS {
+        let res=solution(&fs::read_to_string("test00.txt")?)?;
+        println!("Part 1: {}\nPart 2: {}", res.0, res.1);
+        assert!(res.0=="21");
+        assert!(res.1=="8");
+    }
     Ok(())
 }
 
 fn main() -> Result<()> {
     let input = &fs::read_to_string("input.txt")?;
-    let start = Instant::now();
-    let res = solution(&input)?;
-    println!("Part 1: {}\nPart 2: {}\nRuntime: {}us", res.0, res.1, start.elapsed().as_micros());
+    for (_, solution) in SOLUTIONS.iter().cycle().take(10) {solution(&input)?;} //warmup
+    for (name, solution) in SOLUTIONS {
+        let start = Instant::now();
+        let res = solution(&input)?;
+        println!("---\n{} ({} us)\nPart 1: {}\nPart 2: {}", name, start.elapsed().as_micros(), res.0, res.1, );
+    }
     Ok(())
 }
