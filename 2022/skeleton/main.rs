17dbf272cbd1c337c655a26034dbe84b6ac79512
Janus <janus@insignificancegalore.net>
2022-12-11 07:32:40 +0100

    Make solution loop optional in skeleton

--- 2022/skeleton/main.rs
+++ 2022/skeleton/main.rs
@@ -30,34 +30,57 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
     Ok([part1.to_string(), part2.to_string()])
 }
 
-// Make it simple to compare timing for multiple solutions
-type Solution = dyn Fn(&str) -> Result<[String; 2]>;
-const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
-
 #[test]
 fn test_solution() -> Result<()> {
     let input = &fs::read_to_string("test00.txt")?;
-    for (name, solution) in SOLUTIONS {
-        let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
-        println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
-        assert!(res[0] == "0");
-        assert!(res[1] == "0");
-    }
+    let res = solution(&input)?;
+    println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
+    assert!(res[0] == "0");
+    assert!(res[1] == "0");
     Ok(())
 }
 
 fn main() -> Result<()> {
     let input = &fs::read_to_string("input.txt")?;
-    for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
-        solution(&input)?;
-    } //warmup
-    for (name, solution) in SOLUTIONS {
-        let start = Instant::now();
-        let res = solution(&input)?;
-        println!(
-            "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
-            name, start.elapsed().as_micros(), res[0], res[1],
-        );
-    }
+    for _ in 0..20 {solution(&input)?;} //warmup
+    let start = Instant::now();
+    let res = solution(&input)?;
+    println!(
+        "({} us)\nPart 1: {}\nPart 2: {}",
+        start.elapsed().as_micros(), res[0], res[1],
+    );
     Ok(())
 }
+
+
+// // Make it simple to compare timing for multiple solutions
+// type Solution = dyn Fn(&str) -> Result<[String; 2]>;
+// const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
+
+// #[test]
+// fn test_solution() -> Result<()> {
+//     let input = &fs::read_to_string("test00.txt")?;
+//     for (name, solution) in SOLUTIONS {
+//         let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
+//         println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
+//         assert!(res[0] == "0");
+//         assert!(res[1] == "0");
+//     }
+//     Ok(())
+// }
+
+// fn main() -> Result<()> {
+//     let input = &fs::read_to_string("input.txt")?;
+//     for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
+//         solution(&input)?;
+//     } //warmup
+//     for (name, solution) in SOLUTIONS {
+//         let start = Instant::now();
+//         let res = solution(&input)?;
+//         println!(
+//             "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
+//             name, start.elapsed().as_micros(), res[0], res[1],
+//         );
+//     }
+//     Ok(())
+// }
