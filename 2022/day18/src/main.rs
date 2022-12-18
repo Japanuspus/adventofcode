691a1453df1ced132f3a21fe43de5fde1fc94835
Janus <janus@insignificancegalore.net>
2022-12-18 06:26:11 +0100

    Part 1

---
+++ 2022/day18/src/main.rs
@@ -0,0 +1,100 @@
+#![allow(unused_imports, dead_code)]
+
+use anyhow::{Context, Result};
+use vecmath::{vec2_add, vec3_add};
+use std::{fs, time::Instant, collections::HashSet};
+
+// use parse_display::{Display, FromStr};
+
+// #[derive(Display, FromStr, PartialEq, Debug)]
+// enum Direction {
+//     #[display("forward")]
+//     Forward,
+// }
+
+// #[derive(Debug, Display, FromStr)]
+// #[display("{direction} {distance}")]
+// struct Step {
+//     direction: Direction,
+//     distance: i32,
+// }
+
+type Coord = [i8;3];
+
+fn solution(input_s: &str) -> Result<[String; 2]> {
+    let droplets: HashSet<Coord> = input_s.trim_end()
+        .split("\n")
+        .map(|s| 
+            s.split(',')
+            .map(|v| v.parse::<i8>().with_context(|| format!("Parsing {}", s)))
+            .collect::<Result<Vec<_>,_>>()
+            .and_then(|r| Ok(r.try_into().unwrap()))  //TOOD: proper error map
+        )
+        .collect::<Result<_, _>>()?;
+
+    let nbs: [Coord;6]=[
+        [ 0, 0, 1], [ 0, 0,-1],
+        [ 0, 1, 0], [ 0,-1, 0],
+        [ 1, 0, 0], [-1, 0, 0],
+    ];
+
+    let part1:usize = droplets.iter().map(|d| nbs.iter().filter(|&nb| !droplets.contains(&vec3_add(*d,*nb))).count()).sum();
+    let part2 = 0;
+
+    Ok([part1.to_string(), part2.to_string()])
+}
+
+#[test]
+fn test_solution() -> Result<()> {
+    let input = &fs::read_to_string("test00.txt")?;
+    let res = solution(&input)?;
+    println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
+    assert!(res[0] == "64");
+    assert!(res[1] == "0");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let input = &fs::read_to_string("input.txt")?;
+    for _ in 0..20 {solution(&input)?;} //warmup
+    let start = Instant::now();
+    let res = solution(&input)?;
+    println!(
+        "({} us)\nPart 1: {}\nPart 2: {}",
+        start.elapsed().as_micros(), res[0], res[1],
+    );
+    Ok(())
+}
+
+
+// // Make it simple to compare timing for multiple solutions
+// type Solution = dyn Fn(&str) -> Result<[String; 2]>;
+// const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
+
+// #[test]
+// fn test_solution() -> Result<()> {
+//     let input = &fs::read_to_string("test00.txt")?;
+//     for (name, solution) in SOLUTIONS {
+//         let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
+//         println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
+//         assert!(res[0] == "0");
+//         assert!(res[1] == "0");
+//     }
+//     Ok(())
+// }
+
+// fn main() -> Result<()> {
+//     let input = &fs::read_to_string("input.txt")?;
+//     for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
+//         solution(&input)?;
+//     } //warmup
+//     for (name, solution) in SOLUTIONS {
+//         let start = Instant::now();
+//         let res = solution(&input)?;
+//         println!(
+//             "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
+//             name, start.elapsed().as_micros(), res[0], res[1],
+//         );
+//     }
+//     Ok(())
+// }
