df5fe687da273b301f3f5d49c6afc2b1b92b8332
Janus <janus@insignificancegalore.net>
2022-12-20 14:49:26 +0100

    Part 1 working

--- 2022/day20/src/main.rs
+++ 2022/day20/src/main.rs
@@ -1,20 +1,23 @@
 #![allow(unused_imports, dead_code)]
 
 use anyhow::{Context, Result};
+use itertools::Itertools;
 use std::{fs, time::Instant, collections::{HashMap, HashSet}};
 
-type Ring = HashMap<i32, [i32;2]>;
 
-fn traverse(ring: &Ring, start: i32, d: usize) -> impl Iterator<Item=i32> + '_ {
-    assert!(d<2);
-    itertools::unfold(start, move |i| {
-        *i = ring[&i][d];
-        Some(*i)
-    })
+type Ring = Vec<[u16;2]>;
+
+fn traverse(ring: &Ring, start_index: usize, d: usize) -> impl Iterator<Item=usize> + '_ {
+    itertools::unfold(start_index, move |i| {*i = ring[*i][d] as usize; Some(*i)})
 }
 
-fn print_ring(ring: &Ring) {
-    for v in traverse(ring, 0, 1).take(ring.len()) {
+fn traverse_values<'a>(ring: &'a Ring, values: &'a [i32]) -> impl Iterator<Item=i32> + 'a {
+    let idx0 = values.iter().enumerate().find_map(|(i, v)| if *v==0 {Some(i)} else {None}).unwrap();
+    traverse(ring, idx0, 1).map(|idx| values[idx]).take(values.len())
+}
+
+fn print_ring(ring: &Ring, values: &[i32]) {
+    for v in traverse_values(ring, values) {
         print!("{}, ", &v);
     }
     println!();
@@ -32,39 +35,37 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
         .map(|s| s.parse().with_context(|| format!("Parsing {}", s)))
         .collect::<Result<_, _>>()?;
 
-
     let n = input.len();
-    let mut ring: Ring = input.windows(3).map(|w| (w[1], [w[0], w[2]])).collect();
-    let vals: HashSet<i32> = input.iter().cloned().collect();
-    assert!(vals.len()==input.len());
-    
-    ring.insert(input[n-1], [input[n-2], input[0]]);
-    ring.insert(input[0], [input[n-1], input[1]]);
-    for &i in &input {
-        if i==0 {continue};
-        let (d, dbar) = if i<0 {(0,1)} else {(1, 0)};
+    let mut ring: Vec<[u16;2]> = (0..n).into_iter()
+        .map(|w| [
+            ((w as isize)-1isize).rem_euclid(n as isize) as u16,
+            ((w as isize)+1isize).rem_euclid(n as isize) as u16,
+        ]).collect();
+    if !check_ring(&ring) {
+        panic!();
+    }
+    for (idx, &val) in input.iter().enumerate() {
+        if val==0 {continue};
+        let (d, dbar) = if val<0 {(0,1)} else {(1, 0)};
         // fuse
-        let nbs = *ring.get(&i).unwrap();
-        ring.get_mut(&nbs[0]).unwrap()[1] = nbs[1];
-        ring.get_mut(&nbs[1]).unwrap()[0] = nbs[0];
+        let nbs = ring[idx];
+        ring[nbs[0] as usize][1] = nbs[1];
+        ring[nbs[1] as usize][0] = nbs[0];
         // find insertion point
         let (prev, cur) = itertools::unfold((nbs[dbar], nbs[d]), |(prev, cur)| {
             *prev = *cur;
-            *cur = ring[&cur][d];
+            *cur = ring[*cur as usize][d];
             Some((*prev, *cur))
-        }).skip((i.abs()-1) as usize).next().unwrap();
+        }).nth(val.abs() as usize-1).unwrap();
         // insert
-        ring.get_mut(&prev).unwrap()[d]=i;
-        ring.get_mut(&cur).unwrap()[dbar]=i;
-        ring.insert(i, if d>0 {[prev, cur]} else {[cur, prev]});
-        if !check_ring(&ring) {
-            print_ring(&ring);
-            panic!();
-        };
+        ring[prev as usize][d]=idx as u16;
+        ring[cur as usize][dbar]=idx as u16;
+        ring[idx] = if d>0 {[prev, cur]} else {[cur, prev]};
     }
 
-    println!("{:?}", traverse(&ring, 0, 1).skip(999).step_by(1000).take(3).collect::<Vec<i32>>());
-    let part1:i32 = traverse(&ring, 0, 1).skip(999).step_by(1000).take(3).sum();
+    let idx0 = input.iter().enumerate().find_map(|(i, v)| if *v==0 {Some(i)} else {None}).unwrap();
+    let part1: isize = traverse(&ring, idx0, 1).skip(999).step_by(1000).take(3).map(|idx| input[idx] as isize).sum();
+
     let part2 = 0;
 
     Ok([part1.to_string(), part2.to_string()])
@@ -80,6 +81,16 @@ fn test_solution() -> Result<()> {
     Ok(())
 }
 
+#[test]
+fn test_part1() -> Result<()> {
+    let input = &fs::read_to_string("input.txt")?;
+    let res = solution(&input)?;
+    println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
+    assert!(res[0] == "23321");
+    Ok(())
+}
+
+
 fn main() -> Result<()> {
     let input = &fs::read_to_string("input.txt")?;
     // for _ in 0..20 {solution(&input)?;} //warmup
@@ -93,7 +104,7 @@ fn main() -> Result<()> {
 }
 
 //7160 low
-
+//1646 low
 // // Make it simple to compare timing for multiple solutions
 // type Solution = dyn Fn(&str) -> Result<[String; 2]>;
 // const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
