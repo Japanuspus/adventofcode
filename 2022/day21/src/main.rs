52840ae71142260df50b3beca6bb23100d1e041f
Janus <janus@insignificancegalore.net>
2022-12-21 21:11:46 +0100

    Realized input is tree

--- 2022/day21/src/main.rs
+++ 2022/day21/src/main.rs
@@ -1,46 +1,31 @@
 #![allow(unused_imports, dead_code)]
 
 use anyhow::{Context, Result};
-use std::{fs, time::Instant, collections::HashMap};
+use either::Either;
+use std::{fs, time::Instant, collections::{HashMap, HashSet}};
 
 use parse_display::{Display, FromStr};
 
-type Id = [u8;4];
-fn to_id(s: &str) -> Id {
-    (s.as_bytes()[0..4]).try_into().unwrap()
-}
 
 #[derive(Display, FromStr, PartialEq, Debug)]
-#[display("{lhs} {op} {rhs}")]
-struct Operation<T> {
-    lhs: T,
-    rhs: T,
-    op: char,
-}
-
-#[derive(Display, FromStr, PartialEq, Debug)]
-enum Job<T> {
-    #[display("{0}")]
-    Op(Operation<T>),
+enum Job<T: Sized> {
+    #[display("{lhs} {op} {rhs}")]
+    Op{op: char, lhs: T, rhs: T},
     #[display("{0}")]
     Num(isize),
+    Humn,
 }
 
 #[derive(Display, FromStr, PartialEq, Debug)]
 #[display("{name}: {job}")]
 struct Monkey<T> {
-    name: String,
+    name: T,
     job: Job<T>,
 }
 
-fn compute(a: isize, b: isize, op: char) -> isize {
-    match op {
-        '+' => a+b,
-        '*' => a*b,
-        '-' => a-b,
-        '/' => a/b,
-        _ => {panic!("Invalid operand")}
-    }
+type Id = [u8;4];
+fn to_id(s: &str) -> Id {
+    (s.as_bytes()[0..4]).try_into().unwrap()
 }
 
 fn parse(input_s: &str) -> Result<HashMap<Id, Job<Id>>> {
@@ -53,12 +38,31 @@ fn parse(input_s: &str) -> Result<HashMap<Id, Job<Id>>> {
             .and_then(|m| Ok((
                 to_id(&m.name), 
                 match m.job {
-                    Job::Op(Operation{lhs, rhs, op}) => Job::Op(Operation{lhs: to_id(&lhs), rhs: to_id(&rhs), op}),
+                    Job::Op{lhs, rhs, op} => Job::Op{lhs: to_id(&lhs), rhs: to_id(&rhs), op},
                     Job::Num(v) => Job::Num(v),
+                    Job::Humn => {panic!("Did not expect type Humn in input")},
                 }
             )))
         }).collect::<Result<_, _>>()
 }
+
+fn dependencies(j: &Job<Id>) -> impl Iterator<Item=Id> + '_ {
+    match j {
+        Job::Op{op:_, lhs, rhs} => Either::Left([*lhs, *rhs].into_iter()),
+        _ => Either::Right([].into_iter()),
+    }
+}
+
+fn compute(a: isize, b: isize, op: char) -> isize {
+    match op {
+        '+' => a+b,
+        '*' => a*b,
+        '-' => a-b,
+        '/' => a/b,
+        _ => {panic!("Invalid operand")}
+    }
+}
+
 
 fn solution_1(input_s: &str) -> Result<isize> {
     let jobs = parse(input_s)?;
@@ -73,7 +77,7 @@ fn solution_1(input_s: &str) -> Result<isize> {
     while let Some(&d) = w.iter().rev().next() {
         if values.contains_key(&d) {w.pop(); continue;}
         match &jobs[&d] {
-            Job::Op(Operation{lhs, rhs, op}) => {
+            Job::Op{lhs, rhs, op} => {
                 match (values.get(lhs), values.get(rhs)) {
                     (None, None) => {w.push(*lhs); w.push(*rhs);}
                     (None, _) => {w.push(*lhs);}
@@ -84,20 +88,59 @@ fn solution_1(input_s: &str) -> Result<isize> {
                     }
                 }
             },
-            Job::Num(_) => {panic!("Num monkey not registered in values")}
+            _ => {panic!("Num monkey not registered in values")}
         }
     }
     Ok(values[&to_id("root")])
 }
 
+fn is_tree(jobs: &HashMap<Id, Job<Id>>) -> bool {
+    let mut visited: HashSet<Id> = HashSet::new();
+    let mut work: Vec<Id> = vec![to_id("root")];
+    while let Some(w) = work.pop() {
+        work.extend(dependencies(&jobs[&w]));
+        if !visited.insert(w) {
+            return false
+        }
+    };
+    true
+}
+
 fn solution(input_s: &str) -> Result<[String; 2]> {
     let part1 = solution_1(input_s)?;
 
+    let root = to_id("root");
+    let mut jobs = parse(input_s)?;
+    jobs.insert(to_id("humn"), Job::Humn);
+    if let Some(Job::Op{lhs:_, rhs:_, op}) = jobs.get_mut(&root) {
+        *op = '-';
+    } else {
+        panic!("No OP for root");
+    };
 
+    //let mut stale: Vec::<Id> = vec![root.clone()];
+    // expand multiplications
+    // move divisions out
+    // (a / b) +- d -> (a +- (d * b))/b
+    // (a / b) * d -> (a * d) / b
+    // move operations left. move plus left of minus
+    // (a+b)+(c+d) => ((a+b)+c)+d
+    // (a+b)-
 
     let part2 = 0;
     Ok([part1.to_string(), part2.to_string()])
 }
+
+#[test]
+fn test_tree() -> Result<()> {
+    for file_name in ["test00.txt", "input.txt"] {
+        let input = &fs::read_to_string(file_name)?;
+        let jobs = parse(&input)?;
+        assert!(is_tree(&jobs));    
+    }
+    Ok(())
+}
+
 
 #[test]
 fn test_solution() -> Result<()> {
