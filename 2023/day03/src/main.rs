f3c2b9d81fee0feabb0236c974eadd75a8b88aa9
Janus <janus@insignificancegalore.net>
2023-12-03 08:29:37 +0100

    Part 2 as solved

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -2,41 +2,9 @@
 
 use anyhow::{anyhow, Context, Result};
 use regex::Regex;
-use std::{fs, time::Instant, collections::{HashMap, HashSet}, sync::Arc, iter};
-
-// use parse_display::{Display, FromStr};
-
-// #[derive(Display, FromStr, PartialEq, Debug)]
-// enum Direction {
-//     #[display("forward")]
-//     Forward,
-// }
-
-// #[derive(Debug, Display, FromStr)]
-// #[display("{direction} {distance}")]
-// struct Step {
-//     direction: Direction,
-//     distance: i32,
-// }
+use std::{fs, time::Instant, collections::{HashMap, HashSet, btree_map::{VacantEntry, OccupiedEntry}}, sync::Arc, iter};
 
 fn solution(input_s: &str) -> Result<[String; 2]> {
-    // let symbols: Vec<HashMap<i32, char>> = input_s
-    //     .trim_end()
-    //     .split("\n")
-    //     .map(|s| {
-    //         s.chars().enumerate()
-    //         .filter(|(_, c)| !(*c=='.' || c.is_ascii_digit()))
-    //         .map(|(i, c)| (i as i32, c))
-    //         .collect()
-    //     }).collect();
-
-    // // padded symbol locs to prepare for windowing
-    // let symbol_locs: Vec<HashSet<i32>> = 
-    //     iter::once(HashSet::new())
-    //     .chain(symbols.iter().map(|sl| sl.keys().cloned().collect()))
-    //     .chain(iter::once(HashSet::new()))
-    //     .collect();
-
     let symbols: HashMap<(i32, i32), char> = input_s
         .trim_end()
         .split("\n")
@@ -61,7 +29,27 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
         }        
     }
 
-    let part2 = 0;
+    let mut gears: HashMap<(i32, i32), isize> = HashMap::new();
+    // negative: one nb. positive: product. zero: more than two nbs
+    for (ln, s) in input_s.trim_end().split("\n").enumerate() {
+        let ln=ln as i32;
+        for m in re.find_iter(s) {
+            for dl in -1i32..2 {
+                for x in (m.start() as i32-1)..(m.end() as i32+1) {
+                    let key = (x, ln+dl);
+                    if let Some(c) = symbols.get(&key) {
+                        if c==&'*' {
+                            let val = m.as_str().parse::<usize>()? as isize;
+                            gears.entry(key)
+                            .and_modify(|v| *v = if *v<0 {-*v*val} else {0})
+                            .or_insert(-val);                           
+                        }
+                    }
+                }
+            }
+        }
+    }
+    let part2: isize = gears.values().filter(|&v| *v>0).sum();
 
     Ok([part1.to_string(), part2.to_string()])
 }
@@ -72,7 +60,7 @@ fn test_solution() -> Result<()> {
     let res = solution(&input)?;
     println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
     assert_eq!(res[0], "4361");
-    assert_eq!(res[1], "0");
+    assert_eq!(res[1], "467835");
     Ok(())
 }
 
@@ -91,35 +79,3 @@ fn main() -> Result<()> {
     );
     Ok(())
 }
-
-// // Make it simple to compare timing for multiple solutions
-// type Solution = dyn Fn(&str) -> Result<[String; 2]>;
-// const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
-
-// #[test]
-// fn test_solution() -> Result<()> {
-//     let input = &fs::read_to_string("test00.txt")?;
-//     for (name, solution) in SOLUTIONS {
-//         let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
-//         println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
-//         assert_eq!(res[0], "0");
-//         assert_eq!(res[1], "0");
-//     }
-//     Ok(())
-// }
-
-// fn main() -> Result<()> {
-//     let input = &fs::read_to_string("input.txt")?;
-//     for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
-//         solution(&input)?;
-//     } //warmup
-//     for (name, solution) in SOLUTIONS {
-//         let start = Instant::now();
-//         let res = solution(&input)?;
-//         println!(
-//             "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
-//             name, start.elapsed().as_micros(), res[0], res[1],
-//         );
-//     }
-//     Ok(())
-// }
