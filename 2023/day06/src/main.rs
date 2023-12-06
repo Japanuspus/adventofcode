0ec597cb5a4d6123556fb5506a518eeb874fb09e
Janus <janus@insignificancegalore.net>
2023-12-06 06:18:57 +0100

    Part 1 as solved

---
+++ 2023/day06/src/main.rs
@@ -0,0 +1,97 @@
+#![allow(unused_imports, dead_code)]
+
+use anyhow::{anyhow, Context, Result};
+use std::{fs, time::Instant};
+
+// use parse_display::{Display, FromStr};
+
+// #[derive(Display, FromStr, PartialEq, Debug)]
+// enum Direction {
+//     #[display("forward")]
+//     Forward,
+// }
+
+// #[derive(Debug, Display, FromStr)]
+// #[display("{direction} {distance}")]
+// struct Step {
+//     direction: Direction,
+//     distance: i32,
+// }
+
+fn solution(input_s: &str) -> Result<[String; 2]> {
+    let input: Vec<Vec<i32>> = input_s
+        .trim_end()
+        .split("\n")
+        .map(|s| s.split_ascii_whitespace().skip(1).map(|n| n.parse()).collect::<Result<_,_>>())
+        .collect::<Result<_, _>>()?;
+    // time, dist
+    let races: Vec<(i32, i32)> = input[0].iter().cloned().zip(input[1].iter().cloned()).collect();
+
+    let mut part1: usize = 1;
+    for (time, dist) in races {
+        let n=(0..time).filter(|charge| charge*(time-charge)>dist).count();
+        part1*=n;
+    }
+
+    let part2 = 0;
+
+    Ok([part1.to_string(), part2.to_string()])
+}
+
+#[test]
+fn test_solution() -> Result<()> {
+    let input = &fs::read_to_string("test00.txt")?;
+    let res = solution(&input)?;
+    println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
+    assert_eq!(res[0], "288");
+    assert_eq!(res[1], "0");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let input = &fs::read_to_string("input.txt")?;
+    for _ in 0..20 {
+        solution(&input)?;
+    } //warmup
+    let start = Instant::now();
+    let res = solution(&input)?;
+    println!(
+        "({} us)\nPart 1: {}\nPart 2: {}",
+        start.elapsed().as_micros(),
+        res[0],
+        res[1],
+    );
+    Ok(())
+}
+
+// // Make it simple to compare timing for multiple solutions
+// type Solution = dyn Fn(&str) -> Result<[String; 2]>;
+// const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
+
+// #[test]
+// fn test_solution() -> Result<()> {
+//     let input = &fs::read_to_string("test00.txt")?;
+//     for (name, solution) in SOLUTIONS {
+//         let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
+//         println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
+//         assert_eq!(res[0], "0");
+//         assert_eq!(res[1], "0");
+//     }
+//     Ok(())
+// }
+
+// fn main() -> Result<()> {
+//     let input = &fs::read_to_string("input.txt")?;
+//     for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
+//         solution(&input)?;
+//     } //warmup
+//     for (name, solution) in SOLUTIONS {
+//         let start = Instant::now();
+//         let res = solution(&input)?;
+//         println!(
+//             "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
+//             name, start.elapsed().as_micros(), res[0], res[1],
+//         );
+//     }
+//     Ok(())
+// }
