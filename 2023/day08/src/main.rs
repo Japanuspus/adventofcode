d30e9b1c4d0177bec990bcfbe2500e83b5185905
Janus <janus@insignificancegalore.net>
2023-12-08 06:43:40 +0100

    Part 1 as solved

---
+++ 2023/day08/src/main.rs
@@ -0,0 +1,104 @@
+#![allow(unused_imports, dead_code)]
+
+use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
+use std::{fs, time::Instant, collections::HashMap};
+
+// use parse_display::{Display, FromStr};
+
+// #[derive(Display, FromStr, PartialEq, Debug)]
+// enum Direction {
+//     #[display("forward")]
+//     Forward,
+// }
+
+// #[derive(Debug, Display, FromStr)]
+// #[display("{direction} {distance}")]
+// struct Step {
+//     direction: Direction,
+//     distance: i32,
+// }
+
+fn solution(input_s: &str) -> Result<[String; 2]> {
+    let mut lines = input_s.trim_end().split("\n");
+    let lrs: Vec<u8> = lines.next().unwrap().as_bytes().to_owned();
+    lines.next();
+    // QKX = (SQD, XTJ)
+    let forks: HashMap<String, (String, String)> = lines.map(|ln| {
+        (ln[0..3].to_owned(), (ln[7..10].to_owned(), ln[12..15].to_owned()))
+    }).collect();
+
+    let part1 = lrs.iter().cycle().scan("AAA", |state, step| {
+        let inst = forks.get(*state).unwrap();
+        *state = match step {
+            b'L' => &inst.0,
+            b'R' => &inst.1,
+            _ => panic!("not L or R")
+        };
+        Some(*state)
+    })
+    .take_while_inclusive(|state| *state!="ZZZ")
+    .count();
+    
+    let part2 = 0;
+
+    Ok([part1.to_string(), part2.to_string()])
+}
+
+#[test]
+fn test_solution() -> Result<()> {
+    let input = &fs::read_to_string("test01.txt")?;
+    let res = solution(&input)?;
+    println!("Part 1: {}\nPart 2: {}", res[0], res[1]);
+    assert_eq!(res[0], "6");
+    assert_eq!(res[1], "0");
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let input = &fs::read_to_string("input.txt")?;
+    for _ in 0..20 {
+        solution(&input)?;
+    } //warmup
+    let start = Instant::now();
+    let res = solution(&input)?;
+    println!(
+        "({} us)\nPart 1: {}\nPart 2: {}",
+        start.elapsed().as_micros(),
+        res[0],
+        res[1],
+    );
+    Ok(())
+}
+
+// // Make it simple to compare timing for multiple solutions
+// type Solution = dyn Fn(&str) -> Result<[String; 2]>;
+// const SOLUTIONS: [(&str, &Solution); 1] = [("Original", &solution)];
+
+// #[test]
+// fn test_solution() -> Result<()> {
+//     let input = &fs::read_to_string("test00.txt")?;
+//     for (name, solution) in SOLUTIONS {
+//         let res = solution(&input).with_context(|| format!("Running solution {}", name))?;
+//         println!("---\n{}\nPart 1: {}\nPart 2: {}", name, res[0], res[1]);
+//         assert_eq!(res[0], "0");
+//         assert_eq!(res[1], "0");
+//     }
+//     Ok(())
+// }
+
+// fn main() -> Result<()> {
+//     let input = &fs::read_to_string("input.txt")?;
+//     for (_, solution) in SOLUTIONS.iter().cycle().take(10) {
+//         solution(&input)?;
+//     } //warmup
+//     for (name, solution) in SOLUTIONS {
+//         let start = Instant::now();
+//         let res = solution(&input)?;
+//         println!(
+//             "---\n{} ({} us)\nPart 1: {}\nPart 2: {}",
+//             name, start.elapsed().as_micros(), res[0], res[1],
+//         );
+//     }
+//     Ok(())
+// }
