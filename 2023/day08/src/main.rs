3c0b9ddbbecec26db00af352db30c6712926b780
Janus <janus@insignificancegalore.net>
2023-12-13 09:00:27 +0100

    Day 08 without CRT

--- 2023/day08/src/main.rs
+++ 2023/day08/src/main.rs
@@ -1,55 +1,44 @@
 #![allow(unused_imports, dead_code)]
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use core::fmt;
 use itertools::Itertools;
-use num::BigInt;
-use num::Integer;
+use num::{BigInt, Integer, One};
 use std::{collections::HashMap, fs, time::Instant};
 
-#[test]
-fn test_extended_euclid() {
-    let e = isize::extended_gcd(&240, &46);
-    assert_eq!(e.x, -9);
-    assert_eq!(e.y, 47);
-    assert_eq!(e.gcd, 2);
+mod nm {
+    pub use nom::sequence::*;
+    pub use nom::multi::*;
+    pub use nom::*;
+    pub use nom::error::*;
+    pub use nom::character::complete::*;
+    pub use nom::bytes::complete::*;
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct RSpec<T: Clone> {
-    n: T,
-    a: T,
-}
-
-fn chinese_remainder<T: Integer + Clone + fmt::Debug>(n1: RSpec<T>, n2: RSpec<T>) -> RSpec<T> {
-    let ee = T::extended_gcd(&n1.n, &n2.n);
-    if ee.gcd != T::one() {
-        assert_eq!(n1.a.mod_floor(&ee.gcd), n2.a.mod_floor(&ee.gcd));
-    }
-    let n = n1.n.clone() * n2.n.clone() / ee.gcd.clone();
-    let a = ((n1.a * ee.y * n2.n + n2.a * ee.x * n1.n) / ee.gcd.clone()).mod_floor(&n); // rem_euclid(n);
-    RSpec { n, a }
-}
-
-#[test]
-fn test_chinese_remainder() {
-    let res = chinese_remainder(RSpec::<isize> { n: 3, a: 2 }, RSpec::<isize> { n: 5, a: 1 });
-    assert_eq!(res, RSpec::<isize> { n: 15, a: 11 });
-}
-
-fn solution(input_s: &str) -> Result<[String; 2]> {
-    let mut lines = input_s.trim_end().split("\n");
-    let lrs: Vec<u8> = lines.next().unwrap().as_bytes().to_owned();
-    lines.next();
+fn parse_nom(s: &str) -> nm::IResult<&str, (&str, Vec<Vec<&str>>)> {
+    let (rest, lrs) = nm::terminated(nm::alpha1, nm::multispace1)(s)?;
     // QKX = (SQD, XTJ)
-    let forks: HashMap<String, (String, String)> = lines
-        .map(|ln| {
-            (
-                ln[0..3].to_owned(),
-                (ln[7..10].to_owned(), ln[12..15].to_owned()),
-            )
-        })
-        .collect();
+    let (rest, maps) = nm::separated_list1(
+        nm::newline,
+        nm::terminated(
+            nm::separated_list1(nm::many1(nm::one_of(" (),=")), nm::alpha1),
+            nm::tag(")")
+        )
+    )(rest)?;
+    Ok((rest, (lrs, maps)))
+}    
+
+fn solution(input_s: &str) -> Result<[String;2]> {
+    let (_rest, (lrs, maps)) = parse_nom(input_s).map_err(|e| e.to_owned())?;
+    let lrs: &[u8] = lrs.as_bytes();
+    let forks: HashMap<_,_> = maps.into_iter().map(|w| {
+        //let v: Vec<String> = v0.into_iter().map(|s| s.to_owned()).collect();
+        //(w[0].to_owned(), (w[1].to_owned(), w[2].to_owned()))
+        (w[0],(w[1], w[2]))
+    }).collect();
+
+    // println!("{}", rest);
+    // println!("{:?}", forks);
 
     let part1 = lrs
         .iter()
@@ -66,7 +55,7 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
         .take_while_inclusive(|state| *state != "ZZZ")
         .count();
 
-    let periods: Vec<(usize, usize)> = forks
+    let periods: Vec<usize> = forks
         .keys()
         .filter(|k| k.ends_with('A'))
         .map(|start| {
@@ -87,24 +76,16 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
                 .take(2)
                 .collect();
             assert_eq!(zs[0].1, zs[1].1); //Check that this is a cycle
-            (zs[0].0 + 1, zs[1].0 + 1)
+            assert_eq!(2*(zs[0].0+1), (zs[1].0+1)); //Check that it loops all the way back
+            zs[0].0 + 1
         })
         .collect();
     // for p in &periods {
     //     println!("Period: {:?} {}", p, p.1 - p.0)
     // }
 
-    let common = periods
-        .iter()
-        .map(|(a, n2)| RSpec {
-            n: BigInt::from(n2 - a),
-            a: BigInt::from(*a),
-        })
-        .reduce(|acc, p2| chinese_remainder(acc, p2))
-        .unwrap();
-
-    let part2 = common.a + common.n;
-
+    let part2 = periods.iter()
+        .fold(BigInt::one(), |acc, p| acc.lcm(&BigInt::from(*p)));
     Ok([part1.to_string(), part2.to_string()])
 }
 
