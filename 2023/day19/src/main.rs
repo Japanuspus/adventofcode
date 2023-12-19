c44f07e632500960ca42d3aa5165bcacc2337180
Janus <janus@insignificancegalore.net>
2023-12-19 10:17:53 +0100

    Day 19 as solved

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -74,11 +74,59 @@ fn parse(s: &str) -> nm::IResult<&str, (Vec<WF>, Vec<Material>)> {
     )(s)
 }
 
+type Region = [(u16, u16);4];
+
+const ALL: Region = [(1, 4000);4];
+
+fn region_checked(reg: Region) -> Option<Region> {
+    if reg.iter().any(|r| r.0>r.1) {None} else {Some(reg)}
+}
+
+fn region_intersect(r1: &Region, r2: &Region) -> Option<Region> {
+    let mut res=ALL;
+    for i in 0..4 {
+        res[i] = (r1[i].0.max(r2[i].0), r1[i].1.min(r2[i].1))
+    }
+    region_checked(res)
+}
+
+fn region_split_cond(r: &Region, cond: &Option<(char, char, u16)>) 
+-> (Option<Region>, Option<Region>) {
+    if let Some((an, on, b)) = cond {
+        let mut r_cond = *r;
+        let mut r_other = *r;
+        let i = match an {'x'=>0, 'm'=>1, 'a'=>2, 's'=>3, _=>panic!()};
+        if *on=='>' {
+            r_cond[i].0 = b+1;
+            r_other[i].1 = *b;
+        } else {
+            r_cond[i].1 = b-1;
+            r_other[i].0 = *    b;
+        }
+        (region_checked(r_cond), region_checked(r_other))
+    } else {
+        (Some(*r), None)
+    }
+}
+
+fn region_size(r: &Region) -> usize {
+    r.iter().map(|(a,b)| (b+1-a) as usize).product()
+}
+
+fn union_size(grp: &[Region]) -> usize {
+    grp.iter().map(|r| region_size(r)).sum()
+}
+
+fn add_area(grp: &[Region], a: &Region) -> usize {
+    // additional volume count from adding this region
+    0
+}
+
 fn solution(input_s: &str) -> Result<[String; 2]> {
     let (rest, (wf, materials)) = parse(input_s).map_err(|e| e.to_owned())?;
     assert!(rest.trim().len()==0);
-    
     let workflows: HashMap<&str, Vec<(Option<(char, char, u16)>, &str)>> = wf.into_iter().collect();
+
     let mut part1: usize = 0;
     for m in &materials {
         let mut wf: &str = "in";
@@ -106,8 +154,39 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
         }
     }
 
-    let part2 = 0;
+    // ** part 2 
+    let mut accepted: Vec<Region> = Vec::new();
+    let mut rejected: Vec<Region> = Vec::new();
+    let mut work: Vec<(&str, Region)> = vec![("in", ALL)];
+    while let Some((wf, reg)) = work.pop() {
+        if wf=="A" {
+            accepted.push(reg); 
+            continue;
+        }
+        if wf=="R" {
+            rejected.push(reg); 
+            continue;
+        }
+        let wf_spec = workflows
+            .get(wf).with_context(|| format!("Looking for wf {}", wf))?;
+        let mut remain = reg;
+        for (cond, next_wf) in wf_spec {
+            let (a, b) = region_split_cond(&remain, cond);
+            if let Some(next_reg) = a {
+                work.push((next_wf, next_reg));
+            }
+            if let Some(r) = b {
+                remain=r;
+            } else {
+                break;
+            }
+        }
+    }
 
+    // println!("{} -> {:?}", accepted.len(), &accepted);
+    // println!("{} -> {:?}", rejected.len(), &rejected);
+
+    let part2 = union_size(&accepted[..]);
     Ok([part1.to_string(), part2.to_string()])
 }
 
@@ -123,9 +202,9 @@ fn test_solution() -> Result<()> {
 
 fn main() -> Result<()> {
     let input = &fs::read_to_string("input.txt")?;
-    for _ in 0..20 {
-        solution(&input)?;
-    } //warmup
+    // for _ in 0..20 {
+    //     solution(&input)?;
+    // } //warmup
     let start = Instant::now();
     let res = solution(&input)?;
     println!(
