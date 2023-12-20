8c484f86bb84fe2cab5bc20e04f34cae36948a87
Janus <janus@insignificancegalore.net>
2023-12-20 17:38:30 +0100

    Day 20 as solved

--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -1,27 +1,9 @@
 #![allow(unused_imports, dead_code)]
 
 use anyhow::{anyhow, Context, Result};
-use std::{fs, time::Instant, collections::{BTreeSet, BTreeMap, VecDeque}};
+use num::Integer;
+use std::{fs, time::Instant, collections::{BTreeSet, BTreeMap, VecDeque, HashSet, HashMap}};
 use itertools::Itertools;
-
-use parse_display::{Display, FromStr};
-
-#[derive(Display, FromStr, PartialEq, Debug)]
-enum ModType {
-    #[display("%")]
-    FlipFlop,
-    #[display("&")]
-    Conjunction,
-    #[display(":")]
-    Broadcast,
-}
-
-// #[derive(Debug, Display, FromStr)]
-// #[display("{direction} {distance}")]
-// struct Step {
-//     direction: Direction,
-//     distance: i32,
-// }
 
 mod nm {
     pub use nom::multi::*;
@@ -44,15 +26,10 @@ mod nm {
 }
 //        &nt -> rq, fg, ft, nd, gt, xz
 
-type ModuleSpec<'a>  = ((ModType, &'a str), Vec<&'a str>);
+type ModuleSpec<'a>  = ((Option<char>, &'a str), Vec<&'a str>);
 
 fn parse(s: &str) -> nm::IResult<&str, Vec<ModuleSpec>> {
-    let modtype = nm::map_res(nm::opt(nm::one_of("&%")), |c| match c {
-        Some('&') => Ok(ModType::Conjunction),
-        Some('%') => Ok(ModType::FlipFlop),
-        None => Ok(ModType::Broadcast),
-        _ => Err(anyhow!("Bad modtype: {:?}", c))
-    });
+    let modtype = nm::opt(nm::one_of("&%"));
     let modspec = nm::pair(modtype, nm::alpha1);
     let receivers = nm::separated_list1(nm::ws(nm::char(',')),nm::alpha1);
     let module = nm::separated_pair(
@@ -63,7 +40,7 @@ fn parse(s: &str) -> nm::IResult<&str, Vec<ModuleSpec>> {
 }
 
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum ModuleState<'a> {
     Conjunction(BTreeMap<&'a str, bool>),
     FlipFlop(bool),
@@ -81,6 +58,31 @@ fn update_module_state<'a>(state: &'a mut ModuleState<'a>, src: &'a str, s: bool
     }
 }
 
+fn is_init(m: &ModuleState<'_>) -> bool {
+    match m {
+        ModuleState::Broadcast => true,
+        ModuleState::FlipFlop(v) => !v,
+        ModuleState::Conjunction(mem) => mem.values().all(|m| !*m),
+    }
+}
+
+fn init_modules<'a>(
+    module_specs: &'a Vec<ModuleSpec<'a>>, 
+    inputs: &'a BTreeMap<&str, Vec<&str>>
+) -> BTreeMap<&'a str, (&'a [&'a str], ModuleState<'a>)> {
+    module_specs.iter().map(|((m, src), rcvs)| {
+        let state = match *m {
+            None => ModuleState::Broadcast,
+            Some('&') => ModuleState::Conjunction(
+                inputs[src].iter().map(|&src| (src, false)).collect()
+            ),
+            Some('%') => ModuleState::FlipFlop(false),
+            _ => {panic!()}
+        };
+        (*src, (&rcvs[..], state))
+    }).collect()
+}
+
 fn solution(input_s: &str) -> Result<[String; 2]> {
     let (rest, module_specs) = parse(input_s).map_err(|e| e.to_owned())?;
     assert!(rest.trim_end().len()==0);
@@ -95,17 +97,7 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
     } 
 
     // part 1
-    let mut modules: BTreeMap<&str, (&[&str], ModuleState)> = 
-        module_specs.iter().map(|((m, src), rcvs)| {
-            let state = match m {
-                ModType::Broadcast => ModuleState::Broadcast,
-                ModType::Conjunction => ModuleState::Conjunction(
-                    inputs[src].iter().map(|&src| (src, false)).collect()
-                ),
-                ModType::FlipFlop => ModuleState::FlipFlop(false)
-            };
-            (*src, (&rcvs[..], state))
-        }).collect(); 
+    let mut modules = init_modules(&module_specs, &inputs);
     let mut signals: VecDeque<(&str, bool, &str)> = VecDeque::new(); //src, signal, dst
     let mut signal_count: [usize;2] = [0,0]; //true/false
     for _round in 0..1000 {
@@ -129,33 +121,70 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
                         signals.push_back((dst, output, r));
                     }    
                 }
-            } else {
-                //output module
             }
         }
     }
     let part1 = signal_count[0]*signal_count[1];
 
     // part 2
-    let mut modules: BTreeMap<&str, (&[&str], ModuleState)> = 
-        module_specs.iter().map(|((m, src), rcvs)| {
-            let state = match m {
-                ModType::Broadcast => ModuleState::Broadcast,
-                ModType::Conjunction => ModuleState::Conjunction(
-                    inputs[src].iter().map(|&src| (src, false)).collect()
-                ),
-                ModType::FlipFlop => ModuleState::FlipFlop(false)
-            };
-            (*src, (&rcvs[..], state))
-        }).collect(); 
+    let mut modules = init_modules(&module_specs, &inputs);
+    let src_name = {
+        let src_names = &inputs["rx"]; //rx is not defined as a module
+        assert_eq!(src_names.len(), 1);
+        src_names[0]
+    };
+    {
+        let (_, src_state) = &modules[src_name];
+        assert!(match src_state {ModuleState::Conjunction(_) => true, _=>false});
+    }
+    let ancestor_inputs: BTreeMap<&str, BTreeSet<&str>> = {
+        let rx_input_names: HashSet<&str> = inputs[src_name].iter().cloned().collect();
+        println!("rx <- {} -< {:?}", src_name, rx_input_names);
+        println!("Input ancestors");
+    
+        rx_input_names.iter().map(|&r| {
+        let mut anc = BTreeSet::new();
+        let mut work = vec![r];
+        while let Some(w) = work.pop() {
+            if w=="broadcaster" {continue}
+            if anc.insert(w) {
+                work.extend(inputs[w].iter());
+            }
+        }
+        println!("{} ==> {:?}", r, anc);
+        (r, anc)
+    }).collect()};
+    // are the ancestors distinct?
+    if ancestor_inputs.values().combinations(2)
+        .all(|v| v[0].intersection(v[1]).count()==0) {
+        println!("Ancestors of rx input conjunction are distinct");
+    } else {
+        panic!("Ancestors of rx input conjunction are not distinct");
+    };
+
     let mut signals: VecDeque<(&str, bool, &str)> = VecDeque::new(); //src, signal, dst
-    let mut button_count: usize = 0;
-    'button: loop {
+    // let mut push_period: HashMap<&str, usize> = HashMap::new();
+    let mut loop_detect: Vec<HashMap<Vec<ModuleState>, usize>> 
+        = ancestor_inputs.keys().map(|_| HashMap::new()).collect();
+    let mut loop_result: BTreeMap<&str, [usize;2]> = BTreeMap::new();
+
+    let mut push_count = 0usize;
+    'outer: loop {
+        for (lp, (anc, desc)) in loop_detect.iter_mut().zip(ancestor_inputs.iter()) {
+            let key: Vec<ModuleState> = desc.iter().map(|d| &modules[d].1).cloned().collect();
+            if let Some(previous_push_count) = lp.insert(key, push_count) {
+                //println!("Loop detected {} -- {} for {}", previous_push_count, push_count, anc);
+                loop_result.entry(anc).or_insert([previous_push_count, push_count]);
+            }
+        }
+        if loop_result.len() == loop_detect.len() {break 'outer};
+
         signals.push_back(("in", false, "broadcaster"));
-        button_count+=1;
+        push_count += 1;
         while let Some((src, s, dst)) = signals.pop_front() {
-            //println!("{} {} {}", src, s, dst);
-            if !s && dst=="rx" {break 'button}
+            if dst==src_name && s{
+                println!("Signal@{:4} {} {}->{}", push_count, s, src, dst);
+            }
             if let Some((rcvs, state)) = modules.get_mut(dst) {
                 if let Some(output) = {
                     match state {
@@ -171,12 +200,28 @@ fn solution(input_s: &str) -> Result<[String; 2]> {
                         signals.push_back((dst, output, r));
                     }    
                 }
-            } else {
-                //output module
-            }
+            };    
         }
+
+
+        // for (n, grp) in ancestor_inputs.iter() {
+        //     let n_uninit = grp.iter().filter(|&a| !is_init(&modules[a].1)).count();
+        //     if n_uninit == 0 {
+        //         push_period.entry(n).or_insert(push_count);
+        //         println!("Found period for {} at {}", n, push_count);
+        //     }
+        // }
+        // if push_period.len()==ancestor_inputs.len() {break 'outer};
     }
-    let part2 = button_count;
+
+    println!("Loops:\n{:?}", loop_result);
+
+    // output is in last state before repeat
+    // input state push count
+    // 0 -- 1 -- 2 -...- 3733 --signal--3734(==1)
+    
+    let periods: Vec<_> = loop_result.values().map(|v| v[1]-v[0]).collect();
+    let part2 = periods.into_iter().reduce(|a, b| a.lcm(&b)).unwrap();
 
     Ok([part1.to_string(), part2.to_string()])
 }
