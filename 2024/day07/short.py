70915a0d1b03c0da2145fbfa15e1aa58b1529784
Janus <janus@insignificancegalore.net>
2024-12-07 23:43:43 +0100

    Day 7 explorations

--- 2024/day07/short.py
+++ 2024/day07/short.py
@@ -4,23 +4,20 @@ import operator
 import math
 
 
-def check_eq(eq, ops):
-    res, ns = eq
-    for os in itertools.product(ops, repeat=len(ns)-1):
-        v = ns[0]
-        for b, op in zip(ns[1:], os):
-            v = op(v, b)
-        if v == res:
-            return True
-    return False
-
+def check_rec(res, val, rem, ops) -> bool:
+    if not rem:
+        return val==res
+    if val>res:
+        # all operations increase value - cut if we are past result
+        return False
+    return any(check_rec(res, op(val, rem[0]), rem[1:], ops) for op in ops)
 
 with open("input.txt") as f:
     eqs = [(a, b) for a, *b in ([int(v) for v in re.split(":? ", ln)] 
                                 for ln in f.read().strip().split("\n"))]
 
 print([
-    sum(res for res, ns in eqs if check_eq((res, ns), ops=ops)) for ops in [
+    sum(res for res, ns in eqs if check_rec(res, val=ns[0], rem=ns[1:], ops=ops)) for ops in [
         (operator.add, operator.mul),
-        (operator.add, operator.mul, lambda a, b: a*10**math.ceil(math.log10(b)) + b),
+        (operator.add, operator.mul, lambda a, b: a*10**len(str(b)) + b),
     ]])
--- 2024/day07/solution.py
+++ 2024/day07/solution.py
@@ -15,9 +15,9 @@
 
 # %%
 import re
-import numpy as np
 import itertools
 import operator
+import math
 
 # %%
 with open("input.txt") as f:
@@ -25,7 +25,7 @@ with open("input.txt") as f:
 
 
 # %%
-def check_eq(eq, ops = (operator.add, operator.mul)):
+def check_eq(eq, ops):
     res, ns = eq
     for os in itertools.product(ops, repeat=len(ns)-1):
         v=ns[0]
@@ -37,16 +37,54 @@ def check_eq(eq, ops = (operator.add, operator.mul)):
 
 
 # %%
-sum(res for res, _ in filter(check_eq, eqs))
+# %%time
+ops = (operator.add, operator.mul)
+sum(res for res, ns in eqs if check_eq((res, ns), ops=ops))
 
 
 # %%
-def op_concat(a,b):
-    return a*10**(len(str(b))) + b
+def op_cc(a,b):
+    return a*10**len(str(b))+b
 
-op_concat(11, 123)
+def op_ccm(a,b):
+    return b+a*10**math.ceil(math.log10(b+1))
+# %%
+# %%time
+ops = (operator.add, operator.mul, op_cc)
+sum(res for res, ns in eqs if check_eq((res, ns), ops=ops))
+
+
+# %% [markdown]
+# * * *
+# Faster solution with branch and bound:
 
 # %%
-sum(res for res, ns in eqs if check_eq((res, ns), ops=(operator.add, operator.mul, op_concat)))
+def check_rec(res, val, rem, ops) -> bool:
+    if len(rem)==0:
+        return val==res
+    if val>res:
+        # all operations increase or maintain value - cut if we are past result
+        return False
+    return any(check_rec(res, op(val, rem[0]), rem[1:], ops) for op in ops)
+
+def check_eq_rec(eq, ops) -> bool:
+    res, ns = eq
+    return check_rec(res, val=ns[0], rem=ns[1:], ops=ops)
+
 
 # %%
+ops1 = (operator.add, operator.mul, op_cc)
+ops2 = (operator.add, operator.mul, op_ccm)
+for i,eq in enumerate(eqs):
+    if not check_eq_rec(eq, ops1) == check_eq_rec(eq, ops2):
+        print((i, eq))
+
+# %%
+# %%time
+ops = (operator.add, operator.mul)
+sum(res for res, ns in eqs if check_rec(res, val=ns[0], rem=ns[1:], ops=ops))
+
+# %%
+# %%time
+ops = (operator.add, operator.mul, op_cc)
+sum(res for res, ns in eqs if check_rec(res, val=ns[0], rem=ns[1:], ops=ops))
