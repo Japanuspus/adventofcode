bda6d69daf26ac4b60423bdf8ca572489d43d5ad
Janus <janus@insignificancegalore.net>
2024-12-13 09:21:43 +0100

    Day 13 cleaned up

--- 2024/day13/solution.py
+++ 2024/day13/solution.py
@@ -16,47 +16,20 @@
 # %%
 import re
 import numpy as np
-import itertools
-import math
 
 # %%
 with open(["input.txt", "test00.txt"][0]) as f:
     machines = [[int(v) for v in re.findall(r"\d+", bl)] for bl in f.read().strip().split("\n\n")]
 
-# %%
-t = 0
-for (x1,y1,x2,y2,xt,yt) in machines:
-    m = np.array([[x1, x2], [y1, y2]])
-    b = np.array([xt, yt])
-    a=np.linalg.solve(m,b)
-    aint = np.array([round(v) for v in a])
-    res = m.dot(aint)
-    if np.all(res==b):
-        price = aint.dot([3,1])
-        t+=price
-    else:
-        #print(m, b, a, aint, res, price)
-        pass
-
-print(t)
 
 # %%
-t = 0
-shift = 10000000000000
-for (x1,y1,x2,y2,xt,yt) in machines:
+def tokens(x1,y1,x2,y2,xt,yt, shift=0):
     m = np.array([[x1, x2], [y1, y2]])
     b = np.array([xt+shift, yt+shift])
-    a=np.linalg.solve(m,b)
+    a = np.linalg.solve(m,b)
     aint = np.array([round(v) for v in a])
     res = m.dot(aint)
-    if np.all(res==b):
-        price = aint.dot([3,1])
-        t+=price
-    else:
-        #print(m, b, a, aint, res, price)
-        pass
+    return aint.dot([3,1]) if np.array_equal(res, b) else 0
 
-print(t)
-
-# %%
-math.
+print(sum(tokens(*m) for m in machines))
+print(sum(tokens(*m, shift = 10000000000000) for m in machines))
