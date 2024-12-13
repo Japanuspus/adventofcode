ffbf383c448db3f32a26c6ecf6a19eec47bfa5fe
Janus <janus@insignificancegalore.net>
2024-12-13 09:33:59 +0100

    Day 11 non-recursive

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -14,37 +14,56 @@
 # ---
 
 # %%
-import re
-import numpy as np
-import itertools
-import math
 import functools
+import collections
 
 # %%
 with open(["input.txt", "test00.txt"][0]) as f:
     stones = [int(n) for n in f.read().strip().split(" ")]
 
-# %%
-stones
-
 
 # %%
-def nblink(st, n):
-    if n==0:
-        return 1
-    if st==0:
-        return nblink(1, n-1)
-    elif (sval:=str(st)) and (lsval:=len(sval))%2==0:
-        return nblink(int(sval[0:lsval//2]), n-1)+nblink(int(sval[lsval//2:]), n-1)
-    else:
-        return nblink(st*2024, n-1)
+# %%time
+# 60ms
+@functools.lru_cache(maxsize=1_000_000)
+def bifurcate(s):
+    # watch stone until it splits
+    for n in range(75):
+        if s==0:
+            s=1
+        elif len(str(s))%2==0:
+            sval = str(s)
+            lsval = len(sval)
+            return (n+1, int(sval[0:lsval//2]), int(sval[lsval//2:]))
+        else:
+            s*=2024
+    return None
 
+
+@functools.lru_cache(maxsize=1_000_000)
+def stones_from(s, n):
+    match bifurcate(s):
+        case None:
+            return 1
+        case (nbif,_,_) if nbif>n:
+            return 1
+        case (nbif,_,_) if nbif==n:
+            return 2
+        case (nbif, a, b):
+            return stones_from(a, n-nbif)+stones_from(b, n-nbif)
+
+print([sum(stones_from(s, n) for s in stones) for n in [25, 75]])
+
+
+# %% [markdown]
+# ## Alternative approach without recursion
+#
+# Just wanted to check my intuition on this one. The observation is that the order of stones do not matter, and that the scaling problem is due to a large number of identical stones -- which is why the lru-cache solved the problem in the recursive formulation.
+# Here we just propagate the counts for each stone type
 
 # %%
-sum(nblink(st,25) for st in stones)
-
-
-# %%
+# %%time
+#90ms with generator, 70ms without cache, 40ms with cache
 
 def blink(st):
     if st==0:
@@ -55,109 +74,28 @@ def blink(st):
         yield st*2024
 
 
-def mblink(first_stone, n):
-    stones = [first_stone]
-    for _ in range(n):
-        stones = [s  for stone in stones for s in blink(stone)]
-    return stones
-
-
-
-# %%
-@functools.lru_cache(maxsize=None)
-def blink5(s,m:int):
-    return mblink(s, m)
-
-def process_stone(stone):
-    res = 0
-    plist = [(70,stone)]
-    m=10
-
-    while plist:
-        n, s = plist.pop()
-        print(n, len(plist))
-        if n==5:
-            res+=len(blink5(s, m))
-        else:
-            plist.extend((n-5, sn) for sn in blink5(s, m))
-
-process_stone(1)
-
-
-
-# %% [markdown]
-# new take
-
-# %%
 #@functools.lru_cache(maxsize=1_000_000)
-def bifurcate(s):
-    # watch stone until it splits
-    for n in range(75):
-        if s==0:
-            s=1
-        elif len(str(s))%2==0:
-            sval = str(s)
-            lsval = len(sval)
-            return (n+1, int(sval[0:lsval//2]), int(sval[lsval//2:]))
-        else:
-            s*=2024
-    return None
-
-tot, work = 0, [(25, s) for s in stones]
-bcache = dict()
-while work:
-    n, s = work.pop()
-    if s in bcache:
-        bif = bcache[s]
+def blink(st):
+    if st==0:
+        return (1,)
+    elif (sval:=str(st)) and (lsval:=len(sval))%2==0:
+        return (int(sval[0:lsval//2]), int(sval[lsval//2:]))
     else:
-        bif = bifurcate(s)
-        bcache[s] = bif
-    if bif:
-        nbif, a, b = bif
-        if nbif>n:
-            tot+=1
-        elif nbif==n:
-            tot+=2
-        else:
-            work.extend((n-nbif, v) for v in [a,b])
-    else:
-        tot+=1
+        return (st*2024,)
 
-tot
 
+def stone_prop(n_prop):
+    counts = {s: 1 for s in stones} # no duplicates in input
+    for _ in range(n_prop):
+        new_counts = collections.defaultdict(lambda: 0)
+        for s, n in counts.items():
+            for new_s in blink(s):
+                new_counts[new_s]+=n
+        counts = new_counts
+    return counts
+
+print([sum(stone_prop(n).values()) for n in [25, 75]])
 
 # %%
-@functools.lru_cache(maxsize=1_000_000)
-def bifurcate(s):
-    # watch stone until it splits
-    for n in range(75):
-        if s==0:
-            s=1
-        elif len(str(s))%2==0:
-            sval = str(s)
-            lsval = len(sval)
-            return (n+1, int(sval[0:lsval//2]), int(sval[lsval//2:]))
-        else:
-            s*=2024
-    return None
-
-@functools.lru_cache(maxsize=1_000_000)
-def stones_from(s, n):
-    if bif:=bifurcate(s):
-        nbif, a, b = bif
-        if nbif>n:
-            return 1
-        elif nbif==n:
-            return 2
-        else:
-            return stones_from(a, n-nbif)+stones_from(b, n-nbif)
-    else:
-        return 1
-
-sum(stones_from(s, 25) for s in stones)
-
-# %%
-
-sum(stones_from(s, 75) for s in stones)
 
 # %%
